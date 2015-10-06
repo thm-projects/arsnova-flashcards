0a879cd734acabde25b39df63f6a09d81df82337
Marius Trautrims <trautrims@gmail.com>
2015-10-06 12:10:39 +0200

    welcome screen i18n

--- client/flashcards.js
+++ client/flashcards.js
@@ -3,7 +3,21 @@ Router.route('/', {
 });
 Router.route('/impressum');
 
+getUserLanguage = function () {
+  return navigator.language.substr(0,2);
+};
 
 if (Meteor.isClient) {
+  Meteor.startup(function () {
+    Session.set("showLoadingIndicator", true);
 
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+        Session.set("showLoadingIndicator", false);
+      })
+      .fail(function (error_message) {
+        // Handle the situation
+        console.log(error_message);
+      });
+  });
 }
