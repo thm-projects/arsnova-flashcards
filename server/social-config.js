49220f90cd47812c0df4878111a44aba5fd8a123
Marius Trautrims <trautrims@gmail.com>
2015-10-21 14:19:57 +0200

    Use Settings for Dev and Production

--- server/social-config.js
+++ server/social-config.js
@@ -12,18 +12,18 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '162760697401847',
-    secret: '151daa9c4bc95310e9e1fb90accd70f8'
+    appId: Meteor.settings.facebook.api,
+    secret: Meteor.settings.facebook.secret
 });
 
 ServiceConfiguration.configurations.insert({
     service: 'twitter',
-    consumerKey: 'I8LcQC0NetusL9c5F3EFJUXMl',
-    secret: '2z0kNAtYcSU5lQKk6FDRR60gxuFawB4TM7tWvnU7yKk6EHHpYh'
+    consumerKey: Meteor.settings.twitter.api,
+    secret: Meteor.settings.twitter.secret
 });
 
 ServiceConfiguration.configurations.insert({
     service: 'google',
-    clientId: '131328643116-j00jrgjupbbfeft1a9o0p5ling05vsus.apps.googleusercontent.com',
-    secret: '2-P5Vy6VI0eOrKIlOCZZq8cR'
+    clientId: Meteor.settings.google.api,
+    secret: Meteor.settings.google.secret
 });
