83f01e526054c44ffee97185a5026a6084b30dc1
Marius Trautrims <trautrims@gmail.com>
2015-10-30 17:51:43 +0100

    Implement category counter and make some fixes

--- client/pool/pool.js
+++ client/pool/pool.js
@@ -3,13 +3,20 @@ Template.registerHelper("getCategories", function() {
 });
 
 Template.registerHelper("getCategory", function(value) {
-  var id = value.toString()
+  var id = value.toString();
   if (id.length == 1) id = "0" + id;
   return Categories.findOne(id).name;
 });
 
 Template.category.helpers ({
-  getDecks: function() {
-    return Cardsets.find({ category : this._id });
+  getDecks: function(){
+    var id = parseInt(this._id);
+    return Cardsets.find({category : id});
   }
-})
+});
+
+Template.pool.helpers ({
+  getCount: function(id){
+    return Cardsets.find({category : parseInt(id)}).count();
+  }
+});
