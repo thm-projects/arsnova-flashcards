bdbf9d4f73fef0b6b96e77a8153a0fcd89e92ab2
Matthias Zimny <matthias.zimny@mni.thm.de>
2016-08-15 13:47:34 +0200

    add route to cardset view in admin panel

---
+++ imports/ui/admin_cardsets/admin_cardset.js
@@ -0,0 +1,7 @@
+//------------------------ IMPORTS
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+
+import './admin_cardset.html';
