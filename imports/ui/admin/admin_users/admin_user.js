71a7a363ef8c3d5f9566ae88b874d925741385fd
Matthias Zimny <matthias.zimny@mni.thm.de>
2016-08-21 15:54:16 +0200

    add cardset table to user view in admin panel

--- imports/ui/admin/admin_users/admin_user.js
+++ imports/ui/admin/admin_users/admin_user.js
@@ -5,6 +5,7 @@ import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 
 import { allUsers } from '../../../api/allusers.js';
+import { Cardsets } from '../../../api/cardsets.js';
 
 import './admin_user.html';
 
@@ -39,5 +40,69 @@ Template.admin_user.helpers({
     else {
       return this.lvl;
     }
+  },
+  getOnlineStatus: function() {
+    if (this.status.online === true) {
+      return "Online";
+    }
+    else {
+      return "Offline";
+    }
+  },
+  getLastLogin: function() {
+    return moment(this.status.lastLogin.date).locale(getUserLanguage()).format('LLL');
+  },
+  cardsetListUserAdmin: function () {
+    return Cardsets.find({ owner: this._id });
+  },
+  tableSettings: function() {
+    return {
+      showFilter: false,
+      rowsPerPage: 5,
+      showNavigationRowsPerPage: false,
+      fields: [
+        { key: 'name', label: TAPi18n.__('admin.name') },
+        { key: 'date', label: TAPi18n.__('admin.created'), fn: function(value) {
+            return moment(value).locale(getUserLanguage()).format('LL');
+        }},
+        { key: '_id', label: TAPi18n.__('admin.edit'), sortable: false, cellClass: 'edit', fn: function(value) {
+          return new Spacebars.SafeString("<a id='linkToAdminUserCardset' class='editCardsetAdmin btn btn-xs btn-default' title='" + TAPi18n.__('admin.editcardset') + "' data-cardsetid='" + value + "'><i class='glyphicon glyphicon-pencil'></i></a>");
+        }},
+        { key: 'delete', label: TAPi18n.__('admin.delete'), sortable: false, fn: function() {
+          return new Spacebars.SafeString("<a class='deleteCardsetAdmin btn btn-xs btn-default' title='" + TAPi18n.__('admin.deletecardset') + "' data-toggle='modal' data-target='#cardsetConfirmModalUserAdmin'><i class='glyphicon glyphicon-ban-circle'></i></a>");
+        }}
+      ]
+    }
   }
 });
+
+Template.admin_user.events({
+  'click .reactive-table tbody tr': function(event) {
+    event.preventDefault();
+    var cardset = this;
+
+    if (event.target.className == "deleteCardsetAdmin btn btn-xs btn-default" || event.target.className == "glyphicon glyphicon-ban-circle") {
+      Session.set('cardsetId', cardset._id);
+    }
+  },
+  'click #linkToAdminUserCardset': function(event) {
+    var cardsetid = $(event.currentTarget).data("cardsetid");
+    Router.go('admin_cardset', { _id: cardsetid });
+  },
+});
+
+/**
+ * ############################################################################
+ * cardsetConfirmFormUserAdmin
+ * ############################################################################
+ */
+
+ Template.cardsetConfirmFormUserAdmin.events({
+   'click #cardsetDeleteUserAdmin': function() {
+     var id = Session.get('cardsetId');
+
+     $('#cardsetConfirmModalUserAdmin').on('hidden.bs.modal', function() {
+       Meteor.call("deleteCardset", id);
+     }).modal('hide');
+   }
+ });
