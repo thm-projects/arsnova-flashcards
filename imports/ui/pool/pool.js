915e97aee3140ec68c9fa58f367aa2b38f514998
Yannick Lehnhausen <yannick.lehnhausen@mni.thm.de>
2016-09-12 12:57:16 +0200

    Added active learner functionality to the pool info panel

--- imports/ui/pool/pool.js
+++ imports/ui/pool/pool.js
@@ -5,6 +5,8 @@ import {Template} from 'meteor/templating';
 import {Session} from 'meteor/session';
 import {Authors} from '../../api/authors.js';
 import {Cardsets} from '../../api/cardsets.js';
+import {Learned} from '../../api/learned.js';
+import {Ratings} from '../../api/ratings.js';
 import {Categories} from '../../api/categories.js';
 import {Disciplines} from '../../api/disciplines.js';
 import {Majors} from '../../api/majors.js';
@@ -16,6 +18,8 @@ import {Colleges_Courses} from '../../api/colleges_courses.js';
 import './pool.html';
 
 Meteor.subscribe("cardsets");
+Meteor.subscribe("allLearned");
+
 Meteor.subscribe("allLearned");
 Meteor.subscribe("categories");
 Meteor.subscribe("disciplines");
