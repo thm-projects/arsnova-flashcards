df65156a2ad844b6fcfe3a77b7ee2ae88d82fef3
Curtis Adam <curtis.adam@mni.thm.de>
2016-09-12 23:35:16 +0200

    refacor name translation and fix button position

--- imports/api/cardset.js
+++ imports/api/cardset.js
@@ -7,8 +7,8 @@ Meteor.methods({
 		if (Roles.userIsInRole(id, 'lecturer')) {
 			var colSep = ";"; // Separates columns
 			var newLine = "\r\n"; //Adds a new line
-			var firstName = TAPi18n.__('box_export_firstName');
-			var lastName = TAPi18n.__('box_export_lastName');
+			var birthName = TAPi18n.__('box_export_birth_name');
+			var givenName = TAPi18n.__('box_export_given_name');
 			var eMail = TAPi18n.__('box_export_mail');
 			var boxOne = TAPi18n.__('subject1');
 			var boxTwo = TAPi18n.__('subject2');
@@ -16,7 +16,7 @@ Meteor.methods({
 			var boxFour = TAPi18n.__('subject4');
 			var boxFive = TAPi18n.__('subject5');
 			var boxSix = TAPi18n.__('subject6');
-			var content = lastName + colSep + firstName + colSep + eMail + colSep + boxOne + colSep + boxTwo +
+			var content = givenName + colSep + birthName + colSep + eMail + colSep + boxOne + colSep + boxTwo +
 				colSep + boxThree + colSep + boxFour + colSep + boxFive + colSep + boxSix + newLine;
 			var data = Learned.find({"cardset_id": cardset_id}).fetch();
 			var distinctData = _.uniq(data, false, function (d) {
@@ -24,7 +24,7 @@ Meteor.methods({
 			});
 			for (var i = 0; i < distinctData.length; i++) {
 				var user = Meteor.users.find({"_id": distinctData[i].user_id}).fetch();
-				content += user[0].firstName + colSep + user[0].lastName + colSep + user[0].email + colSep;
+				content += user[0].givenname + colSep + user[0].birthname + colSep + user[0].email + colSep;
 				data = Learned.find({"cardset_id": cardset_id, "user_id": distinctData[i].user_id}).fetch();
 				for (var k = 1; k < 6; k++) {
 					content += Learned.find({
