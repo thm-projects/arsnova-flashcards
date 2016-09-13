032f9fca2283ec9d56b4729ca35a1590dc2bd0be
Dominik Maximilian Krion <dominik.krion@mni.thm.de>
2016-09-13 23:15:34 +0200

    code style

--- server/main.js
+++ server/main.js
@@ -16,18 +16,9 @@ import "../imports/api/upload.js";
 import "../imports/api/allusers.js";
 import "../imports/api/billing.js";
 import "../imports/api/authors.js";
-import "../imports/api/cardsets.js";
-import "../imports/api/categories.js";
-import "../imports/api/cards.js";
 import "../imports/api/disciplines.js";
-import "../imports/api/learned.js";
-import "../imports/api/badges.js";
-import "../imports/api/experience.js";
-import "../imports/api/ratings.js";
 import "../imports/api/majors.js";
 import "../imports/api/modules.js";
-import "../imports/api/notifications.js";
-import "../imports/api/paid.js";
 import "../imports/api/adminSettings.js";
 
 // DATABASE ------------------------------------------------------
