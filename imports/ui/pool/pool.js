95fbf99cca715ef9262fce7065b82a5af24ef238
Dominik Maximilian Krion <dominik.krion@mni.thm.de>
2016-09-14 12:31:16 +0200

    jshint

--- imports/ui/pool/pool.js
+++ imports/ui/pool/pool.js
@@ -3,13 +3,11 @@
 import {Meteor} from "meteor/meteor";
 import {Template} from "meteor/templating";
 import {Session} from "meteor/session";
-import {Authors} from "../../api/authors.js";
 import {Cardsets} from "../../api/cardsets.js";
 import {Colleges_Courses} from '../../api/colleges_courses.js';
 import {Categories} from "../../api/categories.js";
 import {Disciplines} from "../../api/disciplines.js";
 import {Majors} from "../../api/majors.js";
-import {Modules} from "../../api/modules.js";
 import "./pool.html";
 
 Meteor.subscribe("cardsets");
