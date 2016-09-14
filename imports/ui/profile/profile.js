56881c643231a7b77e7ff2e469266ec045a2a062
Yannick Lehnhausen <yannick.lehnhausen@mni.thm.de>
2016-09-14 15:56:01 +0200

    Fixed gulp errors

--- imports/ui/profile/profile.js
+++ imports/ui/profile/profile.js
@@ -28,6 +28,54 @@ Meteor.subscribe('learned', function () {
 	Session.set('data_ready', true);
 });
 
+export function drawGraph() {
+	var card_id = Router.current().params._id;
+	var user_id =  Meteor.userId();
+
+	if (Session.get('data_loaded')) {
+		var box1 = Learned.find({user_id, cardset_id: card_id, box: 1}).fetch().length;
+		var box2 = Learned.find({user_id, cardset_id: card_id, box: 2}).fetch().length;
+		var box3 = Learned.find({user_id, cardset_id: card_id, box: 3}).fetch().length;
+		var box4 = Learned.find({user_id, cardset_id: card_id, box: 4}).fetch().length;
+		var box5 = Learned.find({user_id, cardset_id: card_id, box: 5}).fetch().length;
+		var box6 = Learned.find({user_id, cardset_id: card_id, box: 6}).fetch().length;
+
+		$('#container').highcharts({
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: 'Lernfortschritt'
+			},
+			xAxis: {
+				categories: ['Fach 1', 'Fach 2', 'Fach 3', 'Fach 4', 'Fach 5', 'Gelernt']
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: 'Karten pro Satz'
+				}
+			},
+			legend: {
+				reversed: true
+			},
+			plotOptions: {
+				series: {
+					name: "Lernstand",
+					stacking: 'normal'
+				}
+			},
+			series: [{
+				data: [Number(box1), Number(box2), Number(box3), Number(box4), Number(box5), Number(box6)]
+			}]
+
+		});
+	}
+}
+
+
+
+
 function getLvl() {
 	var user = Meteor.users.findOne(Router.current().params._id);
 	if (user === undefined) {
@@ -1003,46 +1051,14 @@ AppController = RouteController.extend({
 });
 
 Template.profileXp.onRendered(function () {
-	$(function () {
-		var user_id =  Meteor.userId();
-		if (Session.get('data_loaded')) {
-			var box1 = Learned.find({user_id, box: 1}).fetch().length;
-			var box2 = Learned.find({user_id, box: 2}).fetch().length;
-			var box3 = Learned.find({user_id, box: 3}).fetch().length;
-			var box4 = Learned.find({user_id, box: 4}).fetch().length;
-			var box5 = Learned.find({user_id, box: 5}).fetch().length;
-			var box6 = Learned.find({user_id, box: 6}).fetch().length;
-			console.log(box1);
-		}
-
-		$('#container').highcharts({
-			chart: {
-				type: 'column'
-			},
-			title: {
-				text: 'Lernfortschritt'
-			},
-			xAxis: {
-				categories: ['Fach 1', 'Fach 2', 'Fach 3', 'Fach 4', 'Fach 5', 'Gelernt']
-			},
-			yAxis: {
-				min: 0,
-				title: {
-					text: 'Karten pro Satz'
-				}
-			},
-			legend: {
-				reversed: true
-			},
-			plotOptions: {
-				series: {
-					name: "Lernstand aller Kartensätze	",
-					stacking: 'normal'
-				}
-			},
-			series: [{
-				data: [Number(box1), Number(box2), Number(box3), Number(box4), Number(box5), Number(box6)]
-			}]
+	Template.boxSide.onRendered(function () {
+		var self = this;
+		self.subscribe("learned", function () {
+			self.autorun(function () {
+				drawGraph();
+			});
 		});
 	});
+
+
 });
