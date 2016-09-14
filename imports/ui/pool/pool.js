ad1eafe5d2ada2151dec11adfdc3e18c68b2d564
Yannick Lehnhausen <yannick.lehnhausen@mni.thm.de>
2016-09-14 12:49:23 +0200

    fixed merge conflicts

--- imports/ui/pool/pool.js
+++ imports/ui/pool/pool.js
@@ -3,19 +3,13 @@
 import {Meteor} from 'meteor/meteor';
 import {Template} from 'meteor/templating';
 import {Session} from 'meteor/session';
-import {Authors} from '../../api/authors.js';
 import {Cardsets} from '../../api/cardsets.js';
-//import {Learned} from '../../api/learned.js';
-//import {Ratings} from '../../api/ratings.js';
 import {Categories} from '../../api/categories.js';
 import {Disciplines} from '../../api/disciplines.js';
 import {Majors} from '../../api/majors.js';
 import {Modules} from '../../api/modules.js';
 import {getActiveLearner} from '../../ui/cardset/cardset.js';
 import {Colleges_Courses} from '../../api/colleges_courses.js';
-import {Categories} from "../../api/categories.js";
-import {Disciplines} from "../../api/disciplines.js";
-import {Majors} from "../../api/majors.js";
 import "./pool.html";
 
 Meteor.subscribe("cardsets");
