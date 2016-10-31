699012b224d19abdb062ab2a350b93069c923263
Matthias Zimny <matthias.zimny@mni.thm.de>
2016-10-31 13:27:21 +0100

    add browser policy package to prevent xss attacks

---
+++ imports/startup/server/browser-policy.js
@@ -0,0 +1,20 @@
+import {BrowserPolicy} from 'meteor/browser-policy-common';
+
+BrowserPolicy.framing.disallow();
+BrowserPolicy.content.allowInlineScripts();
+BrowserPolicy.content.disallowEval();
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowFontDataUrl();
+
+var trusted = [
+  'https://*.google-analytics.com',
+  'https://*.mxpnl.com',
+  'https://*.zendesk.com',
+  'https://*.braintreegateway.com',
+  'https://git.thm.de',
+  'http://*.cloudflare.com'
+];
+
+_.each(trusted, function(origin) {
+  BrowserPolicy.content.allowOriginForAll(origin);
+});
