e01360ba5844adc4b229e284f7d49bc1cc22677d
Matthias Zimny <matthias.zimny@mni.thm.de>
2016-10-31 14:46:40 +0100

    update chart package to version 2.3.0

--- imports/ui/learn/box.js
+++ imports/ui/learn/box.js
@@ -23,9 +23,57 @@ Meteor.subscribe('learned', function () {
 });
 
 function drawGraph() {
+	var query = {};
+	if (Meteor.userId() !== undefined) {
+		query.user_id = Meteor.userId();
+	}
+	if (Router.current().params._id !== undefined) {
+		query.cardset_id = Router.current().params._id;
+	}
+	query.box = 1;
+	var box1 = Learned.find(query).count();
+	query.box = 2;
+	var box2 = Learned.find(query).count();
+	query.box = 3;
+	var box3 = Learned.find(query).count();
+	query.box = 4;
+	var box4 = Learned.find(query).count();
+	query.box = 5;
+	var box5 = Learned.find(query).count();
+	query.box = 6;
+	var box6 = Learned.find(query).count();
+	var userData = [Number(box1), Number(box2), Number(box3), Number(box4), Number(box5), Number(box6)];
+
 	if (Session.get('data_loaded')) {
 		var ctx = document.getElementById("boxChart").getContext("2d");
-		new Chart(ctx).Bar(Graph(Meteor.userId(), Router.current().params._id), {responsive: true});
+		var boxChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: [TAPi18n.__('subject1'), TAPi18n.__('subject2'), TAPi18n.__('subject3'), TAPi18n.__('subject4'), TAPi18n.__('subject5'), TAPi18n.__('subject6')],
+				datasets: [
+					{
+						backgroundColor: "rgba(242,169,0,0.5)",
+						borderColor: "rgba(74,92,102,0.2)",
+						borderWidth: 1,
+						data: userData,
+						label: 'Anzahl Karten'
+					}
+				]
+			},
+			options: {
+				responsive: true,
+				legend: {
+					display: false
+				},
+				scales: {
+		      yAxes: [{
+		        ticks: {
+		          stepSize: 1
+						}
+					}]
+        }
+			}
+		});
 	}
 }
 
