3e6bff7b5c341cb0e0912fc9af5e5d9b46136a91
jbartelh <joel.bartelheimer@hs-osnabrueck.de>
2016-11-25 21:29:18 +0100

    Jscs fixed

--- imports/ui/main/main.js
+++ imports/ui/main/main.js
@@ -19,101 +19,101 @@ Meteor.subscribe("notifications");
 Meteor.subscribe("adminSettings");
 
 Template.main.events({
-    'click #logout': function (event) {
-        event.preventDefault();
-        Meteor.logout();
-        Router.go('home');
-    },
-    'keyup #input-search': function (event) {
-        event.preventDefault();
-        Session.set("searchValue", $(event.currentTarget).val());
-        if ($(event.currentTarget).val() && CardsetsIndex.search(Session.get("searchValue")).count()) {
-            $('#searchDropdown').addClass("open");
-        } else {
-            $('#searchDropdown').removeClass("open");
-        }
-    },
-    'click #searchResults': function () {
-        $('#searchDropdown').removeClass("open");
-        $('#input-search').val('');
-    },
-    'click #notificationsBtn': function () {
-        var notifications = Notifications.find({read: false, target_type: 'user', target: Meteor.userId()});
-        notifications.forEach(function (notification) {
-            Meteor.call("setNotificationAsRead", notification._id);
-        });
-    },
-    'click #clearBtn': function () {
-        var notifications = Notifications.find({cleared: false, target_type: 'user', target: Meteor.userId()});
-        notifications.forEach(function (notification) {
-            Meteor.call("setNotificationAsCleared", notification._id);
-        });
-    },
-    'click .lang': function (event) {
-        event.preventDefault();
-        TAPi18n.setLanguage($(event.target).data('lang'));
-    }
+	'click #logout': function (event) {
+		event.preventDefault();
+		Meteor.logout();
+		Router.go('home');
+	},
+	'keyup #input-search': function (event) {
+		event.preventDefault();
+		Session.set("searchValue", $(event.currentTarget).val());
+		if ($(event.currentTarget).val() && CardsetsIndex.search(Session.get("searchValue")).count()) {
+			$('#searchDropdown').addClass("open");
+		} else {
+			$('#searchDropdown').removeClass("open");
+		}
+	},
+	'click #searchResults': function () {
+		$('#searchDropdown').removeClass("open");
+		$('#input-search').val('');
+	},
+	'click #notificationsBtn': function () {
+		var notifications = Notifications.find({read: false, target_type: 'user', target: Meteor.userId()});
+		notifications.forEach(function (notification) {
+			Meteor.call("setNotificationAsRead", notification._id);
+		});
+	},
+	'click #clearBtn': function () {
+		var notifications = Notifications.find({cleared: false, target_type: 'user', target: Meteor.userId()});
+		notifications.forEach(function (notification) {
+			Meteor.call("setNotificationAsCleared", notification._id);
+		});
+	},
+	'click .lang': function (event) {
+		event.preventDefault();
+		TAPi18n.setLanguage($(event.target).data('lang'));
+	}
 });
 
 Template.main.helpers({
-    getYear: function () {
-        return moment(new Date()).format("YYYY");
-    },
-    getUsername: function () {
-        if (Meteor.user()) {
-            return Meteor.user().profile.name;
-        }
-    },
-    searchCategories: function () {
-        if (Session.get("searchValue")) {
-            return CardsetsIndex.search(Session.get("searchValue")).fetch();
-        } else {
-            return undefined;
-        }
-    },
-    isActiveProfile: function () {
-        if (ActiveRoute.name(/^profile/)) {
-            return Router.current().params._id === Meteor.userId();
-        }
-        return false;
-    },
-    countNotifications: function () {
-        return Notifications.find({read: false, target_type: 'user', target: Meteor.userId()}).count();
-    },
-    getNotifications: function () {
-        return Notifications.find({cleared: false, target_type: 'user', target: Meteor.userId()}, {sort: {date: -1}});
-    },
-    getLink: function () {
-        return "/cardset/" + this.link_id;
-    },
-    getLanguage: function () {
-        return TAPi18n.getLanguage();
-    },
-    getLanguages: function () {
-        const obj = TAPi18n.getLanguages();
-        const languages = [];
-        for (const key in obj) {
-            if (key) {
-                languages.push({code: key, label: obj[key]});
-            }
-        }
-        if (languages) {
-            return languages;
-        }
-    }
+	getYear: function () {
+		return moment(new Date()).format("YYYY");
+	},
+	getUsername: function () {
+		if (Meteor.user()) {
+			return Meteor.user().profile.name;
+		}
+	},
+	searchCategories: function () {
+		if (Session.get("searchValue")) {
+			return CardsetsIndex.search(Session.get("searchValue")).fetch();
+		} else {
+			return undefined;
+		}
+	},
+	isActiveProfile: function () {
+		if (ActiveRoute.name(/^profile/)) {
+			return Router.current().params._id === Meteor.userId();
+		}
+		return false;
+	},
+	countNotifications: function () {
+		return Notifications.find({read: false, target_type: 'user', target: Meteor.userId()}).count();
+	},
+	getNotifications: function () {
+		return Notifications.find({cleared: false, target_type: 'user', target: Meteor.userId()}, {sort: {date: -1}});
+	},
+	getLink: function () {
+		return "/cardset/" + this.link_id;
+	},
+	getLanguage: function () {
+		return TAPi18n.getLanguage();
+	},
+	getLanguages: function () {
+		const obj = TAPi18n.getLanguages();
+		const languages = [];
+		for (const key in obj) {
+			if (key) {
+				languages.push({code: key, label: obj[key]});
+			}
+		}
+		if (languages) {
+			return languages;
+		}
+	}
 });
 
 Template.main.onRendered(function () {
-    Session.set("searchValue", undefined);
+	Session.set("searchValue", undefined);
 
-    var user = Meteor.users.findOne({
-        _id: Meteor.userId(),
-        lvl: {
-            $exists: false
-        }
-    });
+	var user = Meteor.users.findOne({
+		_id: Meteor.userId(),
+		lvl: {
+			$exists: false
+		}
+	});
 
-    if (user !== undefined) {
-        Meteor.call("initUser");
-    }
+	if (user !== undefined) {
+		Meteor.call("initUser");
+	}
 });
