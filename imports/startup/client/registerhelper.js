ce4c851153c1b5c94dd290f905fdfa6ce4f6671f
Dennis Schönhof <dennis.schoenhof@mni.thm.de>
2016-11-29 16:00:59 +0100

    Fix style errors

--- imports/startup/client/registerhelper.js
+++ imports/startup/client/registerhelper.js
@@ -125,24 +125,24 @@ Template.registerHelper("getAuthorName", function (owner) {
 });
 
 Template.registerHelper("getAuthor", function (owner) {
-    var author = Meteor.users.findOne({"_id": owner});
-    if (author) {
-        var degree = "";
-        if (author.profile.title) {
-            degree = author.profile.title;
-        } else {
-        	degree = TAPi18n.__('navbar-collapse.none');
+	var author = Meteor.users.findOne({"_id": owner});
+	if (author) {
+		var degree = "";
+		if (author.profile.title) {
+			degree = author.profile.title;
+		} else {
+			degree = TAPi18n.__('navbar-collapse.none');
 		}
-        if (author.profile.givenname === undefined && author.profile.birthname === undefined) {
-            author.profile.givenname = TAPi18n.__('cardset.info.undefinedAuthor');
-            return author.profile.givenname;
-        }
-        return {
-        	degree: degree,
+		if (author.profile.givenname === undefined && author.profile.birthname === undefined) {
+			author.profile.givenname = TAPi18n.__('cardset.info.undefinedAuthor');
+			return author.profile.givenname;
+		}
+		return {
+			degree: degree,
 			givenname: author.profile.givenname,
 			birthname: author.profile.birthname
-		}
-    }
+		};
+	}
 });
 
 // Return the cardset license
