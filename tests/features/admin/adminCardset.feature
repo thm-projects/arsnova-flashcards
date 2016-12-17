35e5480a403b7b409258aecb5b917c601f0af053
Florian Naumann <florian.naumann@mni.thm.de>
2016-12-17 11:40:37 +0100

    Inserted helper functions

--- tests/features/admin/adminCardset.feature
+++ tests/features/admin/adminCardset.feature
@@ -1,16 +1,16 @@
 Feature: Delete a cardset in the back end
 
   As an admin,
-  so that the a cardset is not in the system anymore,
+  so that the cardset is not in the system anymore,
   the users wants to delete it in the backend.
 
   Background:
     Given user is on the site
-    And the user is logged in
-    And the user is in the back end
+    And user is logged in
+    And user is in the back end
 
 @watch
   Scenario: Admin deletes cardset
-    When the user goes to the menu item cardsets
-    And the user clicks on the delete button
+    When user goes to the menu item cardsets
+    And user clicks on the delete button
     Then the cardset should not be in the list anymore
--- tests/features/admin/adminCardset_steps.js
+++ tests/features/admin/adminCardset_steps.js
@@ -1,42 +1,42 @@
+import {login, agreeCookies, logoutAdmin} from "../helper_functions"
+
 module.exports = function() {
     'use strict';
 
 	var numberOfCardsets = 2;
 
-    this.Given(/^User is on the site$/, function () {
+    this.Given(/^user is on the site$/, function () {
         browser.url('http://localhost:3000');
     });
 
-    this.Given(/^User is logged in$/, function () {
-       var SetUsername = function (name) {
-		$('#TestingBackdorUsername').val(name);
-	};
-	client.execute(SetUsername, "testuser");
-	browser.click('a[id="BackdoorLogin"]');
-	browser.click('a[id="BackdoorLogin"]');
+    this.Given(/^user is logged in$/, function () {
+       	login("testuser");
+	agreeCookies();
     });
 
-    this.Given(/^User is in the back end$/, function () {
+    this.Given(/^user is in the back end$/, function () {
         browser.waitForExist("#adminpanel", 5000);
 	browser.url('http://localhost:3000/admin/dashboard');
     });
 
-    this.When(/^User goes to the menu item cardsets$/, function () {
+    this.When(/^user goes to the menu item cardsets$/, function () {
         browser.url('http://localhost:3000/admin/cardsets');
     });
 
-    this.When(/^User clicks on the delete button$/, function () {
+    this.When(/^user clicks on the delete button$/, function () {
 	browser.waitForExist('.delete',5000);
 
         browser.click(".deleteCardsetAdmin");
+	browser.waitForVisible("#cardsetConfirmModalAdmin", 5000);
 	browser.click("#cardetDeleteAdmin");
     });
 
     this.Then(/^the cardset should not be in the list anymore$/, function () {
-	browser.waitForVisible("#cardsetConfirmModalAdmin", 5000, true);    	
-	browser.waitForExist('.delete',5000);
+	browser.waitForVisible("#cardsetConfirmModalAdmin", 5000, true);
 	
 	var elements = browser.elements(".delete");
 	expect(elements.value.length-1).toEqual(numberOfCardsets-1);
+
+	logoutAdmin();
     });
 };
