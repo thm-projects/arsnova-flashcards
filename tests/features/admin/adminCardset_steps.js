31e85aee81b07219a30816bbbc3b8639f4ecbeb4
Florian Naumann <florian.naumann@mni.thm.de>
2016-12-17 11:57:50 +0100

    Fixed styling

--- tests/features/admin/adminCardset_steps.js
+++ tests/features/admin/adminCardset_steps.js
@@ -1,42 +1,42 @@
-import {login, agreeCookies, logoutAdmin} from "../helper_functions"
+import {login, agreeCookies, logoutAdmin} from "../helper_functions";
 
-module.exports = function() {
-    'use strict';
+module.exports = function () {
+	'use strict';
 
 	var numberOfCardsets = 2;
 
-    this.Given(/^user is on the site$/, function () {
-        browser.url('http://localhost:3000');
-    });
+	this.Given(/^user is on the site$/, function () {
+		browser.url('http://localhost:3000');
+	});
 
-    this.Given(/^user is logged in$/, function () {
-       	login("testuser");
-	agreeCookies();
-    });
+	this.Given(/^user is logged in$/, function () {
+		login("testuser");
+		agreeCookies();
+	});
 
-    this.Given(/^user is in the back end$/, function () {
-        browser.waitForExist("#adminpanel", 5000);
-	browser.url('http://localhost:3000/admin/dashboard');
-    });
+	this.Given(/^user is in the back end$/, function () {
+		browser.waitForExist("#adminpanel", 5000);
+		browser.url('http://localhost:3000/admin/dashboard');
+	});
 
-    this.When(/^user goes to the menu item cardsets$/, function () {
-        browser.url('http://localhost:3000/admin/cardsets');
-    });
+	this.When(/^user goes to the menu item cardsets$/, function () {
+		browser.url('http://localhost:3000/admin/cardsets');
+	});
 
-    this.When(/^user clicks on the delete button$/, function () {
-	browser.waitForExist('.delete',5000);
+	this.When(/^user clicks on the delete button$/, function () {
+		browser.waitForExist('.delete',5000);
 
-        browser.click(".deleteCardsetAdmin");
-	browser.waitForVisible("#cardsetConfirmModalAdmin", 5000);
-	browser.click("#cardetDeleteAdmin");
-    });
+		browser.click(".deleteCardsetAdmin");
+		browser.waitForVisible("#cardsetConfirmModalAdmin", 5000);
+		browser.click("#cardetDeleteAdmin");
+	});
 
-    this.Then(/^the cardset should not be in the list anymore$/, function () {
-	browser.waitForVisible("#cardsetConfirmModalAdmin", 5000, true);
+	this.Then(/^the cardset should not be in the list anymore$/, function () {
+		browser.waitForVisible("#cardsetConfirmModalAdmin", 5000, true);
 	
-	var elements = browser.elements(".delete");
-	expect(elements.value.length-1).toEqual(numberOfCardsets-1);
+		var elements = browser.elements(".delete");
+		expect(elements.value.length - 1).toEqual(numberOfCardsets - 1);
 
-	logoutAdmin();
-    });
+		logoutAdmin();
+	});
 };
