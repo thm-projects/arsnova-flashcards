81d55be1b165413644ab291504b05c5c5cf2d8a1
Kevin Stroeher <kevin.stroeher@googlemail.com>
2016-12-17 12:07:07 +0100

    - Fix firstLogin missing username - Add logoutAdmin function

--- tests/features/helper_functions.js
+++ tests/features/helper_functions.js
@@ -15,22 +15,30 @@ export function logout() {
 	browser.click('#logout');
 }
 
+/* exported agreeCookies */
 export function agreeCookies() {
 	browser.setCookie({name: 'cookieconsent_dismissed', value: 'yes'});
 }
 
 /* exported firstLogin */
-export function firstLogin() {
-	login();
+export function firstLogin(username) {
+	login(username);
 	browser.waitForExist('#accept_checkbox', 5000);
 	browser.$('#accept_checkbox').click();
 	browser.click('button[id="accept_button"]');
 }
 
+/* exported setResolution */
 export function setResolution() {
 	browser.setViewportSize({
 		width: 1920,
 		height: 1080
 	});
 	browser.windowHandleSize();
+}
+
+/* exported logoutAdmin */
+export function logoutAdmin() {
+	browser.waitForExist('#logout_admin', 5000);
+	browser.click('#logout_admin');
 }
