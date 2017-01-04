0031d7475e974676c3e9d1a503e0dc909fd1f899
Timo Martin <Timo.Martin@mni.thm.de>
2017-01-04 14:35:37 +0100

    Navbar toggler only shown on login. Navbar toggler window closes on item click.

--- imports/ui/main/main.js
+++ imports/ui/main/main.js
@@ -19,6 +19,12 @@ Meteor.subscribe("Users");
 Meteor.subscribe("notifications");
 Meteor.subscribe("adminSettings");
 
+$(document).on('click','.navbar-collapse.in',function (e) {
+	if ($(e.target).is('a')) {
+		$(this).collapse('hide');
+	}
+});
+
 Template.main.events({
 	'click #logout': function (event) {
 		event.preventDefault();
