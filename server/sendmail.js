045dc0f71de0a450a70627f320fc8ea51b21027c
Dennis Schönhof <dennis.schoenhof@mni.thm.de>
2017-01-05 19:02:40 +0100

    Change Newsletter language to german

--- server/sendmail.js
+++ server/sendmail.js
@@ -20,9 +20,9 @@ export class MailNotifier {
 			var active = Learned.findOne({cardset_id: cardset._id, user_id: user_id, active: true});
 			var deadline = new Date(active.currentDate.getTime() + cardset.daysBeforeReset * 86400000);
 			if (deadline.getTime() > cardset.learningEnd.getTime()) {
-				return (TAPi18n.__('deadlinePrologue') + cardset.learningEnd.toLocaleDateString() + TAPi18n.__('deadlineEpilogue1'));
+				return (TAPi18n.__('deadlinePrologue', null, Meteor.settings.newsletterLanguage) + cardset.learningEnd.toLocaleDateString() + TAPi18n.__('deadlineEpilogue1', null, Meteor.settings.newsletterLanguage));
 			} else {
-				return (TAPi18n.__('mailNotification.textDate1') + deadline.toLocaleDateString() + TAPi18n.__('mailNotification.textDate2'));
+				return (TAPi18n.__('mailNotification.textDate1', null, Meteor.settings.newsletterLanguage) + deadline.toLocaleDateString() + TAPi18n.__('mailNotification.textDate2', null, Meteor.settings.newsletterLanguage));
 			}
 		}
 	}
@@ -33,18 +33,18 @@ export class MailNotifier {
 		} else {
 			var notifier = new Notifications();
 			var cards = notifier.getActiveCardsCount(cardset._id, user_id);
-			var subject = TAPi18n.__('mailNotification.subjectTitle');
-			var text = TAPi18n.__('mailNotification.textIntro') + notifier.getName(user_id) + TAPi18n.__('mailNotification.textIntro1') + TAPi18n.__('mailNotification.newCards1');
+			var subject = TAPi18n.__('mailNotification.subjectTitle', null, Meteor.settings.newsletterLanguage);
+			var text = TAPi18n.__('mailNotification.textIntro', null, Meteor.settings.newsletterLanguage) + notifier.getName(user_id) + TAPi18n.__('mailNotification.textIntro1', null, Meteor.settings.newsletterLanguage) + TAPi18n.__('mailNotification.newCards1', null, Meteor.settings.newsletterLanguage);
 
 			if (cards === 1) {
-				subject += TAPi18n.__('mailNotification.subjectSingular1') + cards + TAPi18n.__('mailNotification.subjectSingular2');
-				text += cards + TAPi18n.__('mailNotification.newCards2Singular');
+				subject += TAPi18n.__('mailNotification.subjectSingular1', null, Meteor.settings.newsletterLanguage) + cards + TAPi18n.__('mailNotification.subjectSingular2', null, Meteor.settings.newsletterLanguage);
+				text += cards + TAPi18n.__('mailNotification.newCards2Singular', null, Meteor.settings.newsletterLanguage);
 			} else {
-				subject += TAPi18n.__('mailNotification.subjectPlural1') + cards + TAPi18n.__('mailNotification.subjectPlural2');
-				text += cards + TAPi18n.__('mailNotification.newCards2Plural');
+				subject += TAPi18n.__('mailNotification.subjectPlural1', null, Meteor.settings.newsletterLanguage) + cards + TAPi18n.__('mailNotification.subjectPlural2', null, Meteor.settings.newsletterLanguage);
+				text += cards + TAPi18n.__('mailNotification.newCards2Plural', null, Meteor.settings.newsletterLanguage);
 			}
-			subject += TAPi18n.__('mailNotification.subjectCardset') + cardset.name + TAPi18n.__('mailNotification.subjectEnd');
-			text += cardset.name + TAPi18n.__('mailNotification.subjectEnd');
+			subject += TAPi18n.__('mailNotification.subjectCardset', null, Meteor.settings.newsletterLanguage) + cardset.name + TAPi18n.__('mailNotification.subjectEnd', null, Meteor.settings.newsletterLanguage);
+			text += cardset.name + TAPi18n.__('mailNotification.textEnd', null, Meteor.settings.newsletterLanguage);
 			this.sendMail(this.getMail(user_id), subject, text, cardset._id, "#3d9c19", "#328114");
 		}
 	}
@@ -54,25 +54,26 @@ export class MailNotifier {
 			throw new Meteor.Error("not-authorized");
 		} else {
 			var notifier = new Notifications();
-			var subject = TAPi18n.__('mailNotification.subjectReset') + cardset.name;
-			var text = TAPi18n.__('mailNotification.textIntro') + notifier.getName(user_id) + "\n\n" + TAPi18n.__('mailNotification.mailCard') + cardset.name + TAPi18n.__('mailNotification.mailCard1') + "\n\n";
+			var subject = TAPi18n.__('mailNotification.subjectReset', null, Meteor.settings.newsletterLanguage) + cardset.name;
+			var text = TAPi18n.__('mailNotification.textIntro', null, Meteor.settings.newsletterLanguage) + notifier.getName(user_id) + "\n\n" + TAPi18n.__('mailNotification.mailCard', null, Meteor.settings.newsletterLanguage) + cardset.name + TAPi18n.__('mailNotification.mailCard1', null, Meteor.settings.newsletterLanguage) + "\n\n";
 			text += this.getDeadline(cardset, user_id);
 			this.sendMail(this.getMail(user_id), subject, text, cardset._id, "#d70000", "#a40000");
 		}
 	}
 
 	sendMail (mail, subject, text, cardsetId, titleColor, buttonColor) {
-		var datenschutz = TAPi18n.__('footer.datenschutz');
-		var agb = TAPi18n.__('footer.agb');
-		var impressum = TAPi18n.__('footer.impressum');
-		var service = TAPi18n.__('mailNotification.service');
-		var unsubscribe = TAPi18n.__('mailNotification.unsubscribe');
-		var copyright = TAPi18n.__('mailNotification.copyright');
+		var datenschutz = TAPi18n.__('footer.datenschutz', null, Meteor.settings.newsletterLanguage);
+		var agb = TAPi18n.__('footer.agb', null, Meteor.settings.newsletterLanguage);
+		var impressum = TAPi18n.__('footer.impressum', null, Meteor.settings.newsletterLanguage);
+		var service = TAPi18n.__('mailNotification.service', null, Meteor.settings.newsletterLanguage);
+		var unsubscribe = TAPi18n.__('mailNotification.unsubscribe', null, Meteor.settings.newsletterLanguage);
+		var copyright = TAPi18n.__('mailNotification.copyright', null, Meteor.settings.newsletterLanguage);
+		var myCardset = TAPi18n.__('mailNotification.my-cardset', null, Meteor.settings.newsletterLanguage);
 		if (!Meteor.isServer) {
 			throw new Meteor.Error("not-authorized");
 		} else {
 			if (mail) {
-				var html = SSR.render("newsletter", {message: text, title: subject, id: cardsetId, url: Meteor.settings.public.rooturl, titlecolor: titleColor, btncol: buttonColor, datenschutz: datenschutz, impressum: impressum, agb: agb, service: service, unsubscribe: unsubscribe, copyright: copyright});
+				var html = SSR.render("newsletter", {message: text, title: subject, id: cardsetId, url: Meteor.settings.public.rooturl, titlecolor: titleColor, btncol: buttonColor, datenschutz: datenschutz, impressum: impressum, agb: agb, service: service, unsubscribe: unsubscribe, copyright: copyright, cardset: myCardset});
 				Email.send({
 					from: '',
 					to: mail,
