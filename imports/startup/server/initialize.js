e4293525927d1d6c0ecaae078f29a2359de537a2
Curtis Adam <curtis.adam@mni.thm.de>
2017-02-13 14:57:41 +0100

    Update cards with missing difficulty settings to difficulty 0 on server start

--- imports/startup/server/initialize.js
+++ imports/startup/server/initialize.js
@@ -1,5 +1,6 @@
 import {Meteor} from "meteor/meteor";
 import {Badges} from "../../api/badges.js";
+import {Cards} from "../../api/cards.js";
 import {ColorThemes} from "../../api/theme.js";
 import {AdminSettings} from "../../api/adminSettings";
 import {CronScheduler} from "../../../server/cronjob.js";
@@ -8,13 +9,13 @@ var initColorThemes = function () {
 	return [{
 		"_id": "default",
 		"name": "Default"
-	},{
+	}, {
 		"_id": "aflatunense",
 		"name": "Aflatunense"
-	},{
+	}, {
 		"_id": "lemonchill",
 		"name": "Lemon Chill"
-	},{
+	}, {
 		"_id": "bluestar",
 		"name": "Blue Star"
 	}];
@@ -147,6 +148,19 @@ Meteor.startup(function () {
 			name: "mailSettings",
 			enabled: false
 		});
+	}
+
+	var cards = Cards.find({difficulty: {$exists: false}}).fetch();
+	for (var i = 0; i < cards.length; i++) {
+		Cards.update({
+				_id: cards[i]._id
+			},
+			{
+				$set: {
+					difficulty: 0
+				}
+			}
+		);
 	}
 
 	if (Badges.find().count() === 0) {
