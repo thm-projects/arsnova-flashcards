676767288220eb2be0993f0c98204bd8ca9d3aa2
Curtis Adam <curtis.adam@mni.thm.de>
2017-08-10 14:33:04 +0200

    Refactor chimp test scripts

---
+++ tests/dumpTestDatabase.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIRECTORY=`dirname $0`
+CALLED_FROM_SCRIPT=true
+
+source $DIRECTORY/helpers/scriptSettings.sh
+
+checkDirectory
+
+declare -a BLACKLISTEDFILES=("adminSettings.bson" "badges.bson" "colorThemes.bson" "cronHistory.bson" "experience.bson"
+"notifications.bson" "system.indexes.bson" "webPushSubscriptions.bson" "roles.bson"
+"meteor_accounts_loginServiceConfiguration.bson" "meteor_oauth_pendingCredentials.bson"
+"meteor_oauth_pendingRequestTokens.bson")
+
+#Dump the Test-Database
+echo -e $GREEN"Dumping the Test-Database..." $NC
+if ! mongodump --quiet -h "$MONGO_HOST" --port "$MONGO_PORT" -d "$MONGO_DB" -o $dumpDir 1> /dev/null; then
+	echo -e $RED"mongodump failed!" $NC
+	exit 2
+fi
+rm $restoreDir/*.json
+for files in "${BLACKLISTEDFILES[@]}"; do
+    rm $restoreDir"/$files"
+done
+echo -e $GREEN"Dumped Test-Database." $NC
+exit 0
