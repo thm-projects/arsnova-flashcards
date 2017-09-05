c387a6e352a730348d85278d34e1b8598a2c06a4
Curtis Adam <curtis.adam@mni.thm.de>
2017-09-05 20:19:02 +0200

    Backend settings: Use THM colors and disable test buttons while a test is in progress

--- imports/ui/admin/admin_settings/admin_settings.js
+++ imports/ui/admin/admin_settings/admin_settings.js
@@ -12,34 +12,46 @@ Template.admin_settings.events({
 	'click #setNotificationTarget': function (evt, tmpl) {
 		Meteor.call('changeNotificationTarget', tmpl.find('#notificationTarget').value, function (error, result) {
 			if (error || result === false) {
-				$('#setTargetInfo').css({'visibility': 'visible', 'color': '#b94a48'});
+				$('#setTargetInfo').css({'visibility': 'visible', 'color': '#9C132D'});
 				$('#setTargetInfo').html(TAPi18n.__('admin-settings.test-notifications.targetError'));
 			} else {
-				$('#setTargetInfo').css({'visibility': 'visible', 'color': '#4ab948'});
+				$('#setTargetInfo').css({'visibility': 'visible', 'color': '#78B925'});
 				$('#setTargetInfo').html(TAPi18n.__('admin-settings.test-notifications.setSuccess'));
 			}
 		});
 	},
 	'click #sendMail': function () {
+		$('#sendMail').attr('disabled','disabled');
+		$('#sendWeb').attr('disabled','disabled');
+		$('#sendInfo').css({'visibility': 'visible', 'color': '#F5AA01'});
+		$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.waitingForResponse'));
 		Meteor.call('sendTestMail', function (error) {
 			if (error) {
-				$('#sendInfo').css({'visibility': 'visible', 'color': '#b94a48'});
+				$('#sendInfo').css({'visibility': 'visible', 'color': '#9C132D'});
 				$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.sendMailError'));
 			} else {
-				$('#sendInfo').css({'visibility': 'visible', 'color': '#4ab948'});
+				$('#sendInfo').css({'visibility': 'visible', 'color': '#78B925'});
 				$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.sendMailSuccess'));
 			}
+			$('#sendMail').removeAttr('disabled');
+			$('#sendWeb').removeAttr('disabled');
 		});
 	},
 	'click #sendWeb': function () {
+		$('#sendMail').attr('disabled','disabled');
+		$('#sendWeb').attr('disabled','disabled');
+		$('#sendInfo').css({'visibility': 'visible', 'color': '#F5AA01'});
+		$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.waitingForResponse'));
 		Meteor.call('sendTestWebNotification', function (error) {
 			if (error) {
-				$('#sendInfo').css({'visibility': 'visible', 'color': '#b94a48'});
+				$('#sendInfo').css({'visibility': 'visible', 'color': '#9C132D'});
 				$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.sendWebError'));
 			} else {
-				$('#sendInfo').css({'visibility': 'visible', 'color': '#4ab948'});
+				$('#sendInfo').css({'visibility': 'visible', 'color': '#78B925'});
 				$('#sendInfo').html(TAPi18n.__('admin-settings.test-notifications.sendWebSuccess'));
 			}
+			$('#sendMail').removeAttr('disabled');
+			$('#sendWeb').removeAttr('disabled');
 		});
 	}
 });
