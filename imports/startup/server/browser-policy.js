4288493e78f5b15a163840c695a406eae1f2b667
Curtis Adam <curtis.adam@mni.thm.de>
2017-12-21 13:03:59 +0100

    Revert: Add disallowInlineScripts policy

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,7 +1,6 @@
 import {BrowserPolicy} from 'meteor/browser-policy-common';
 
 BrowserPolicy.framing.disallow();
-BrowserPolicy.content.disallowInlineScripts();
 BrowserPolicy.content.disallowEval();
 BrowserPolicy.content.allowInlineStyles();
 BrowserPolicy.content.allowFontDataUrl();
