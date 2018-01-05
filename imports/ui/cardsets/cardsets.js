69e45b728659a21b58fd56784902225c7d70633f
Curtis Adam <curtis.adam@mni.thm.de>
2018-01-05 23:53:45 +0100

    Add three new rows to 'Mein Lernpensum': 'Lernphase', 'Heute zu lernen' and 'Lernmodus'

--- imports/ui/cardsets/cardsets.js
+++ imports/ui/cardsets/cardsets.js
@@ -197,6 +197,59 @@ Template.cardsetRow.helpers({
 	},
 	getLink: function (cardset_id) {
 		return ActiveRoute.name('shuffle') ? "#" : ("/cardset/" + cardset_id);
+	},
+	getLearnphaseStatus: function () {
+		if (this.learningActive) {
+			return TAPi18n.__('set-list.activeLearnphase');
+		} else {
+			return TAPi18n.__('set-list.inactiveLearnphase');
+		}
+	},
+	getWorkload: function () {
+		let actualDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+		actualDate.setHours(0, 0, 0, 0);
+		let count = Leitner.find({
+			cardset_id: this._id,
+			user_id: Meteor.userId(),
+			active: true
+		}).count() + Wozniak.find({
+			cardset_id: this._id, user_id: Meteor.userId(), nextDate: {
+				$lte: actualDate
+			}
+		}).count();
+		switch (count) {
+			case 0:
+				return TAPi18n.__('set-list.noCardsToLearn');
+			case 1:
+				return TAPi18n.__('set-list.cardsToLearn');
+			default:
+				return count + TAPi18n.__('set-list.cardsToLearnPlural');
+		}
+	},
+	getLearningMode: function () {
+		let actualDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+		actualDate.setHours(0, 0, 0, 0);
+		let count = 0;
+		if (Leitner.find({cardset_id: this._id, user_id: Meteor.userId(), active: true}).count()) {
+			count += 1;
+		}
+		if (Wozniak.find({
+				cardset_id: this._id, user_id: Meteor.userId(), nextDate: {
+					$lte: actualDate
+				}
+			}).count()) {
+			count += 2;
+		}
+		switch (count) {
+			case 0:
+				return TAPi18n.__('set-list.none');
+			case 1:
+				return TAPi18n.__('set-list.leitner');
+			case 2:
+				return TAPi18n.__('set-list.wozniak');
+			case 3:
+				return TAPi18n.__('set-list.both');
+		}
 	}
 });
 
