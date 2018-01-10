a24fd8422af634ce9f129f721f9526b095ea8246
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2018-01-10 20:08:29 +0100

    Optimize PWA metrics

--- server/main.js
+++ server/main.js
@@ -1,33 +1,5 @@
 // STARTUP -------------------------------------------------------
-import "../imports/startup/server/accounts-config.js";
-import "../imports/startup/server/initialize.js";
-import "../imports/startup/server/browser-policy.js";
-import "../imports/api/cardsets.js";
-import "../imports/api/cards.js";
-import "../imports/api/learned.js";
-import "./leitner.js";
-import "./wordcloud.js";
-import "../imports/api/experience.js";
-import "../imports/api/ratings.js";
-import "../imports/api/notifications.js";
-import "../imports/api/paid.js";
-import "../imports/api/userdata.js";
-import "../imports/api/markdown.js";
-import "../imports/api/upload.js";
-import "../imports/api/allusers.js";
-import "../imports/api/billing.js";
-import "../imports/api/adminSettings.js";
-import "../imports/api/cardsetUserlist.js";
-import "../imports/api/cardsets.js";
-import "../imports/api/cards.js";
-import "../imports/api/colleges_courses.js";
-import "../imports/api/experience.js";
-import "../imports/api/ratings.js";
-import "../imports/api/notifications.js";
-import "../imports/api/paid.js";
-import "../imports/api/adminSettings.js";
-import "../imports/api/webPushSubscriptions.js";
 
-// DATABASE ------------------------------------------------------
+import { WebApp } from 'meteor/webapp';
 
-// SERVER METHODS ------------------------------------------------
+WebApp.addHtmlAttributeHook(() => ({lang: 'de'}));
