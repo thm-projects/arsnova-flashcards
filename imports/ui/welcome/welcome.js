bbb4a0feb2687c201b69bd41d8cf4bc077492c62
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2018-03-12 18:49:08 +0100

    further style adjustments

--- imports/ui/welcome/welcome.js
+++ imports/ui/welcome/welcome.js
@@ -56,15 +56,16 @@ function createTagCloud() {
 	document.getElementById('cards-welcome-image').height = $('.color-cards').height();
 	let textScale = 1.2;
 	//let wordRotation = 0.7853981634;
-	let gridSize = Math.round(16 * $('#tag-cloud-container').width() / 512);
-	let weightFactor = Math.pow(textScale, 2.3) * $('#tag-cloud-container').width() / 512;
+	let gridSize = Math.round(16 * $('#tag-cloud-container').width() / 1440);
+	let weightFactor = Math.pow(textScale, 2.3) * $('#tag-cloud-container').width() / 450;
 	WordCloud(document.getElementById('tag-cloud-canvas'),
 		{
 			list: list,
 			gridSize: gridSize,
 			weightFactor: weightFactor,
+			minSize: 14,
 			//shape: "diamond",
-			drawOutOfBound: true,
+			drawOutOfBound: false,
 			rotateRatio: 0,
 			//rotationSteps: 2,
 			//minRotation: -wordRotation,
