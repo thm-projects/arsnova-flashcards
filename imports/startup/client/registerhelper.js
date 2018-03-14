1d2649acc9b9a626df38c9ecff6b54ee4bb1d29a
Curtis Adam <curtis.adam@mni.thm.de>
2018-03-14 17:10:41 +0100

    Refactor sass color names and use a white background for cards without a difficulty

--- imports/startup/client/registerhelper.js
+++ imports/startup/client/registerhelper.js
@@ -12,7 +12,7 @@ import {getAuthorName} from "../../api/cardsetUserlist.js";
 import {toggleFullscreen} from "../../ui/card/card";
 import {Paid} from "../../api/paid";
 import {getUserLanguage} from "../../startup/client/i18n";
-import {gotNotesForDifficultyLevel} from "../../api/cardTypes";
+import {gotDifficultyLevel, gotNotesForDifficultyLevel} from "../../api/cardTypes";
 
 Meteor.subscribe("collegesCourses");
 
@@ -382,6 +382,13 @@ Template.registerHelper("getLearnphase", function (state) {
 });
 
 Template.registerHelper("getCardBackground", function (difficulty, cardType, backgroundStyle) {
+	if (!gotDifficultyLevel(cardType)) {
+		if (backgroundStyle === 1) {
+			return 'box-difficultyLined0';
+		} else {
+			return 'box-difficultyBlank0';
+		}
+	}
 	if (difficulty === 0 && !gotNotesForDifficultyLevel(cardType)) {
 		difficulty = 1;
 	}
