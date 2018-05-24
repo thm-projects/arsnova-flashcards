dbbd95f6c79df233c1f798061502bc08649ac8bb
Curtis Adam <curtis.adam@mni.thm.de>
2018-05-24 13:18:15 +0200

    Add an extra new line to markdeep content

--- imports/startup/client/registerhelper.js
+++ imports/startup/client/registerhelper.js
@@ -558,7 +558,7 @@ Template.registerHelper("getMaximumText", function (text) {
 const helper = new MeteorMathJax.Helper({
 	useCache: true,
 	transform: function (x) {
-		x += "\n";
+		x += "\n\n";
 		x = window.markdeep.format(x, true);
 		x = DOMPurify.sanitize(x, DOMPurifyConfig);
 		x = lib.setLightBoxes(x);
