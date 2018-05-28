42379fc5c10c0827181fcc0dea665af3a2d68615
Curtis Adam <curtis.adam@mni.thm.de>
2018-05-28 13:02:00 +0200

    Use an extra template for the hint preview

--- imports/ui/card/card.js
+++ imports/ui/card/card.js
@@ -1021,6 +1021,7 @@ Template.cardHint.helpers({
 		return (Session.get('activeEditMode') === 2 && isEditModeOrPresentation());
 	}
 });
+
 /*
  * ############################################################################
  * cardHintContent
@@ -1184,6 +1185,25 @@ Template.cardLectureContent.helpers({
 	}
 });
 
+/*
+ * ############################################################################
+ * cardHintContentPreview
+ * ############################################################################
+ */
+
+Template.cardHintContentPreview.helpers({
+	getPlaceholder: function (mode) {
+		if (isPresentation()) {
+			return CardType.getPlaceholderText(mode, this.cardType);
+		}
+	},
+	gotHint: function () {
+		return this.hint !== '' && this.hint !== undefined;
+	},
+	isCentered: function () {
+		return isCentered(2, this.centerTextElement, this.cardType);
+	}
+});
 
 /*
  * ############################################################################
@@ -1351,7 +1371,7 @@ Template.flashcards.helpers({
 		return TAPi18n.__('learning-goal.level' + (this.learningGoalLevel + 1));
 	},
 	gotHint: function () {
-		return CardType.gotHint(this.cardType);
+		return (CardType.gotHint(this.cardType) && this.hint !== "" && this.hint !== undefined);
 	},
 	gotDictionary: function () {
 		return CardType.gotDictionary(this.cardType);
