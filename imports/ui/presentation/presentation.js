c78da4e27337fb5eacbd52ebb5dab111313a623d
Curtis Adam <curtis.adam@mni.thm.de>
2018-05-29 10:14:48 +0200

    Turn presentation analog clock into a "Minutensprunguhr"

--- imports/ui/presentation/presentation.js
+++ imports/ui/presentation/presentation.js
@@ -17,7 +17,7 @@ function updatePresentationClock() {
 	let min = date.getMinutes();
 	let sec = date.getSeconds();
 	let hrPosition = hr * 360 / 12 + ((min * 360 / 60) / 12);
-	let minPosition = (min * 360 / 60) + (sec * 360 / 60) / 60;
+	let minPosition = min * 360 / 60;
 	let secPosition = sec * 360 / 60;
 	$("#hour").css("transform","rotate(" + hrPosition + "deg)");
 	$("#minute").css("transform","rotate(" + minPosition + "deg)");
