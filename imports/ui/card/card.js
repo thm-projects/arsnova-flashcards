7781a48e8b0bcdea79f1d8e3c10db317e1c63853
Curtis Adam <curtis.adam@mni.thm.de>
2018-06-01 15:47:02 +0200

    Dynamicly calculate the quantity of shuffled cardsets to stay in sync with its references

--- imports/ui/card/card.js
+++ imports/ui/card/card.js
@@ -1436,7 +1436,18 @@ Template.flashcards.helpers({
 	},
 	getCardsetCount: function (getQuantityValue) {
 		if (getQuantityValue) {
-			return Cardsets.findOne({_id: Router.current().params._id}).quantity;
+			let cardset = Cardsets.findOne(Router.current().params._id);
+			if (cardset.shuffled) {
+				let quantity = 0;
+				cardset.cardGroups.forEach(function (cardset_id) {
+					if (cardset_id !== Router.current().params._id) {
+						quantity += Cardsets.findOne(cardset_id).quantity;
+					}
+				});
+				return quantity;
+			} else {
+				return cardset.quantity;
+			}
 		} else {
 			return Cards.find({cardset_id: Router.current().params._id}).count();
 		}
