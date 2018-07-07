0595ac4505e070b7e03ff9dfb529728edc233c1f
Curtis Adam <curtis.adam@mni.thm.de>
2018-07-07 16:09:21 +0200

    Adjust shuffled cardset counter

--- imports/ui/card/header/item/countCards.js
+++ imports/ui/card/header/item/countCards.js
@@ -17,28 +17,16 @@ Template.cardHeaderItemCountCards.helpers({
 	},
 	getCardsetCount: function (getQuantityValue) {
 		if (getQuantityValue) {
-			let cardset;
 			if (Route.isDemo()) {
-				cardset = Cardsets.findOne({kind: 'demo', shuffled: true});
+				return Cards.findOne({kind: 'demo', shuffled: true}).count();
 			} else {
-				cardset = Cardsets.findOne(Router.current().params._id);
-			}
-			if (cardset.shuffled) {
-				let quantity = 0;
-				cardset.cardGroups.forEach(function (cardset_id) {
-					if (cardset_id !== Router.current().params._id) {
-						quantity += Cardsets.findOne(cardset_id).quantity;
-					}
-				});
-				return quantity;
-			} else {
-				return cardset.quantity;
+				return Cards.find({cardset_id: Router.current().params._id}).count();
 			}
 		} else {
 			if (Route.isDemo()) {
-				return Cardsets.findOne({kind: 'demo', shuffled: true}).count();
+				return Cardsets.findOne({kind: 'demo', shuffled: true}).quantity;
 			} else {
-				return Cards.find({cardset_id: Router.current().params._id}).count();
+				return Cardsets.findOne({_id: Router.current().params._id}).quantity;
 			}
 		}
 	}
