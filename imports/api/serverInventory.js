fe4a780c21660d7b679793c8c3a08be21414eca8
Curtis Adam <curtis.adam@mni.thm.de>
2018-07-09 15:57:39 +0200

    Don't include server and demo cardsets, cards and users in the server inventory list

--- imports/api/serverInventory.js
+++ imports/api/serverInventory.js
@@ -4,9 +4,15 @@ import {Cards} from "./cards";
 
 if (Meteor.isServer) {
 	Meteor.publish('serverInventory', function () {
-		Counts.publish(this, 'cardsetsCounter', Cardsets.find(), {fastCount: true});
-		Counts.publish(this, 'cardsCounter', Cards.find(), {fastCount: true});
-		Counts.publish(this, 'usersCounter', Meteor.users.find(), {fastCount: true});
+		Counts.publish(this, 'cardsetsCounter', Cardsets.find({kind: {$nin: ['server', 'demo']}}), {fastCount: true});
+		Counts.publish(this, 'cardsCounter', Cards.find({
+			cardset_id: {
+				$in: Cardsets.find({kind: {$nin: ['server', 'demo']}}).map(function (cardset) {
+					return cardset._id;
+				})
+			}
+		}), {fastCount: true});
+		Counts.publish(this, 'usersCounter', Meteor.users.find({_id: {$nin: ['NotificationsTestUser']}}), {fastCount: true});
 		Counts.publish(this, 'usersOnlineCounter', Meteor.users.find({'status.online': true}), {fastCount: true});
 	});
 }
