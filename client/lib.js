7044539309d10dc2cf83a0ccbb81b31ef7e6bcce
Curtis Adam <curtis.adam@mni.thm.de>
2018-08-04 17:20:26 +0200

    Enable image navigation between images that are inside the same card content

--- client/lib.js
+++ client/lib.js
@@ -4,6 +4,7 @@
  * */
 export function setLightBoxes(content) {
 	var element = $(content);
+	let item_id = Math.random().toString(36).substr(2);
 	$(element).find('img').each(function () {
 		let imageTitleElement = $(this).closest('.image');
 		imageTitleElement = imageTitleElement.last();
@@ -11,7 +12,7 @@ export function setLightBoxes(content) {
 		console.log(imageTitle);
 		let imageUrl = $(this).attr('src');
 		$(this).attr('data-type', 'cardImage');
-		let wrapped = $(this).wrap('<div class="lightbox-container"><a href="' + imageUrl + '" class="lightbox-img" title="' + imageTitle + '" target="_blank" data-lightbox="' + imageUrl + '"></a></div>').parent().prop('outerHTML');
+		let wrapped = $(this).wrap('<div class="lightbox-container"><a href="' + imageUrl + '" class="lightbox-img" title="' + imageTitle + '" target="_blank" data-lightbox="' + item_id + '"></a></div>').parent().prop('outerHTML');
 		$(this).text(wrapped);
 	});
 
