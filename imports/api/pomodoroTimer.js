ccd9109e5b12b439c731e436f4483b0da61e8fd2
Fabian Handstein <fabian.handstein@mni.thm.de>
2018-09-10 15:44:16 +0200

    fixed Pomodoro Intervall function

--- imports/api/pomodoroTimer.js
+++ imports/api/pomodoroTimer.js
@@ -27,7 +27,7 @@ let breakRunning = false;
 
 export let PomodoroTimer = class PomodoroTimer {
 	/*The following code snippet is a life saver and was found on stack overflow. It allows you to draw an arc around a circle in svg using only the polar coordinates.*/
-	static polarToCartesian (centerX, centerY, radius, angleInDegrees) {
+	static polarToCartesian(centerX, centerY, radius, angleInDegrees) {
 		let angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
 		return {
@@ -36,7 +36,7 @@ export let PomodoroTimer = class PomodoroTimer {
 		};
 	}
 
-	static describeArc (x, y, radius, startAngle, endAngle) {
+	static describeArc(x, y, radius, startAngle, endAngle) {
 		let start = this.polarToCartesian(x, y, radius, endAngle);
 		let end = this.polarToCartesian(x, y, radius, startAngle);
 
@@ -51,12 +51,12 @@ export let PomodoroTimer = class PomodoroTimer {
 	}
 
 	/*this function rotates the hands of the clock based on the time.*/
-	static r (el, deg) {
+	static r(el, deg) {
 		el.setAttribute('transform', 'rotate(' + deg + ' 50 50)');
 	}
 
 	/*the arcs around the clock get redrawn every second, as do the hands on the clock, thanks to this setInterval function. It runs every second.*/
-	static interval () {
+	static interval() {
 		/*here, we get the current time, and since there are 360 degrees around a circle, and 60 minutes in an hour, each minute is 360/60 = 6 degrees of rotation. multiply that by the number of minutes and add the seconds and their corresponding degree value and you get a minute hand that moves every second. Similar with the hour hand.*/
 		let d = new Date();
 		this.r(document.getElementById("pomodoroMin"), 6 * d.getMinutes() + d.getSeconds() / 10);
@@ -99,6 +99,21 @@ export let PomodoroTimer = class PomodoroTimer {
 			document.getElementById("breakArc").setAttribute("d", this.describeArc(0, 0, 0, 0, 0));
 
 			/*the first sweet alert! This is what pops up when you finish a pomodoro. It congradulates the user and lets them start their break when they are ready. There is no option to stop the session in this box, that function is relegated to the second click on the clock, as noted by the title.*/
+			swal({
+					title: "Good job!",
+					text: "You have completed <b>" + totalPoms + " pomodoros</b> of work! Are you ready for a relaxing <b>" + breakLength + " minutes</b> of whatever the heck you feel like?",
+					type: "success",
+					html: true,
+					confirmButtonText: "Yes, I can't wait!"
+				},
+
+				/*and this is what runs when the user clicks the confirm button on the popup. It starts the break, and gets the current time and sets the end from there.*/
+				function () {
+					breakRunning = true;
+					var popTime = new Date();
+					breakBeginAngle = 6 * popTime.getMinutes() + popTime.getSeconds() / 10;
+					endBreak = 6 * popTime.getMinutes() + popTime.getSeconds() / 10 + 6 * breakLength;
+				});
 		}
 
 		/*this is what runs every second while the break is active. It just draws the break arc and the progress arc behind it.*/
@@ -124,11 +139,27 @@ export let PomodoroTimer = class PomodoroTimer {
 			} else if (totalPoms % 4 === 0) {
 				breakLength /= 3;
 			}
+
+			swal({
+					title: "Break's over!",
+					text: "Hope that break left you feeling refreshed! Now it's time to get back to accomplishing your goals. Ready for<b> " + pomLength + " more minutes</b> of work?",
+					html: true,
+					confirmButtonText: "Let me at it!"
+				},
+
+				/*starts the work cycle up again, automatically.*/
+				function () {
+					pomRunning = true;
+					var popTime = new Date();
+					endPom = 6 * popTime.getMinutes() + popTime.getSeconds() / 10 + 6 * pomLength;
+					endBreak = endPom + 6 * breakLength;
+					pomBeginAngle = 6 * popTime.getMinutes() + popTime.getSeconds() / 10;
+				});
 		}
 	}
 
 	/*updates the sentence in the modal under the place where you set your goal for the session with the correct total amount of time it will take to complete that many pomodoros. With correct grammar, which is why it's such a long function.*/
-	static updateTimeParagraph () {
+	static updateTimeParagraph() {
 		let longBreak = breakLength * 3;
 		let isAnd = "";
 		let hourAmmount = Math.floor(((pomLength + breakLength) * goalPoms + (Math.floor(goalPoms / 4) * longBreak)) / 60);
@@ -154,7 +185,7 @@ export let PomodoroTimer = class PomodoroTimer {
 	}
 
 	/*if not in a session, clicking the clock opens the start up modal to begin one, and if you are in a session, clicking pops up a warning dialog before exiting the session  */
-	static clickClock () {
+	static clickClock() {
 		/*okay, so I tried this pen on my android phone and no sounds would play. Turns out that you need to attach sounds to a click function or they won't run on chrome for android. This empty sound allows the success and failure sounds to play, but not the bell sound. Oh well, this app wouldn't work on a phone anyway, because the screen would lock and exit it.*/
 		document.getElementById("chromeMobile").play();
 		if (pomRunning || breakRunning) {
@@ -233,20 +264,20 @@ export let PomodoroTimer = class PomodoroTimer {
 	}
 
 	/*gets the goal number of pomodoros*/
-	static updatePomNumSlider () {
+	static updatePomNumSlider() {
 		$('#pomQuantity').val($('#pomNumSlider').val());
 		goalPoms = $('#pomNumSlider').val();
 		this.updateTimeParagraph();
 	}
 
-	static updatePomQuantity () {
+	static updatePomQuantity() {
 		$('#pomNumSlider').val($('#pomQuantity').val());
 		goalPoms = $('#pomQuantity').val();
 		this.updateTimeParagraph();
 	}
 
 	/*hides the goal box, shows the place where you can change the pomodoro length*/
-	static updateSettingsBtn () {
+	static updateSettingsBtn() {
 		$("#settings").toggle();
 		$("#goalDiv").toggle();
 		if ($("#modalTitle").html() === "Choose your goal!") {
@@ -257,7 +288,7 @@ export let PomodoroTimer = class PomodoroTimer {
 	}
 
 	/*when you update the work slider or input box or the break ones, it updates the total time and makes sure you didn't go over 60 minutes total work and break time per cycle. I could probably refactor all the following code. Someday!*/
-	static updateWorkLength () {
+	static updateWorkLength() {
 		$('#workSlider').val($('#workLength').val());
 		pomLength = parseInt($('#workLength').val(), 10);
 
@@ -269,7 +300,7 @@ export let PomodoroTimer = class PomodoroTimer {
 		this.updateTimeParagraph();
 	}
 
-	static updateWorkSlider () {
+	static updateWorkSlider() {
 		$('#workLength').val($('#workSlider').val());
 		pomLength = parseInt($('#workSlider').val(), 10);
 		if (pomLength + breakLength > 60) {
@@ -280,7 +311,7 @@ export let PomodoroTimer = class PomodoroTimer {
 		this.updateTimeParagraph();
 	}
 
-	static updatePlayLength () {
+	static updatePlayLength() {
 		$('#playSlider').val($('#playLength').val());
 		breakLength = parseInt($('#playLength').val(), 10);
 		if (pomLength + breakLength > 60) {
@@ -291,7 +322,7 @@ export let PomodoroTimer = class PomodoroTimer {
 		this.updateTimeParagraph();
 	}
 
-	static updatePlaySlider () {
+	static updatePlaySlider() {
 		$('#playLength').val($('#playSlider').val());
 		breakLength = parseInt($('#playSlider').val(), 10);
 		if (pomLength + breakLength > 60) {
@@ -301,8 +332,9 @@ export let PomodoroTimer = class PomodoroTimer {
 		}
 		this.updateTimeParagraph();
 	}
+
 	/*any way you close the modal, by clicking the close button, confirm button, or clicking outside the box, starts a session. Makes it faster when you just want to start working. This initializes the end positions of all the arcs, and changes the instructions at the top of the screen.*/
-	static start () {
+	static start() {
 		let curTime = new Date();
 		endPom = (6 * curTime.getMinutes() + curTime.getSeconds() / 10 + 6 * pomLength);
 		endBreak = (endPom + 6 * breakLength);
