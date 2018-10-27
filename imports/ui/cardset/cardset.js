19c217af97add2b0aec46050b386721d61f75fe3
Curtis Adam <curtis.adam@mni.thm.de>
2018-10-27 16:30:35 +0200

    Display number of Pomodori determined by instructor (below daily workload)

--- imports/ui/cardset/cardset.js
+++ imports/ui/cardset/cardset.js
@@ -1600,6 +1600,20 @@ Template.learningPhaseInfoBox.helpers({
 			return this.maxCards + " " + TAPi18n.__('confirmLearn-form.cards');
 		}
 	},
+	getPomodoroCount: function () {
+		return TAPi18n.__('pomodoro.cardsetInfo.count.content', {
+			count: this.pomodoroTimer.quantity,
+			link: TAPi18n.__('pomodoro.form.link'),
+			tooltip: TAPi18n.__('pomodoro.form.tooltip.link', {pomodoro: TAPi18n.__('pomodoro.name')}),
+			pomodoro: TAPi18n.__('pomodoro.name', {count: this.pomodoroTimer.quantity})
+		});
+	},
+	getPomodoroWorkTime: function () {
+		return TAPi18n.__('pomodoro.cardsetInfo.work.content', {minutes: TAPi18n.__('pomodoro.form.time.minute', {count: this.pomodoroTimer.workLength})});
+	},
+	getPomodoroBreakTime: function () {
+		return TAPi18n.__('pomodoro.cardsetInfo.break.content', {minutes: TAPi18n.__('pomodoro.form.time.minute', {count: this.pomodoroTimer.breakLength})});
+	},
 	canJoinBonus: function () {
 		return Bonus.canJoinBonus(Session.get('activeCardset')._id);
 	},
