f3240d5a5d44e2a367d291e4ed70d82270a1a434
Curtis Adam <curtis.adam@mni.thm.de>
2018-11-11 14:58:45 +0100

    Refactor WebPushNotifications

--- imports/api/webPushSubscriptions.js
+++ imports/api/webPushSubscriptions.js
@@ -4,6 +4,47 @@ import webPush from 'web-push';
 
 export const WebPushSubscriptions = new Mongo.Collection("webPushSubscriptions");
 
+export let WebPushNotifications = class WebPushNotifications {
+	/**
+	 * Creates a web push subscription for the current device.
+	 * The Browser ask the user for permissions and creates the subscription.
+	 * Afterwards the subscription will be saved for the current user via the
+	 * Meteor-method addWebPushSubscription.
+	 */
+	static subscribeForPushNotification () {
+		try {
+			navigator.serviceWorker.getRegistration()
+				.then(function (registration) {
+					return registration.pushManager.getSubscription()
+						.then(function () {
+							return registration.pushManager.subscribe({userVisibleOnly: true});
+						});
+				})
+				.then(function (subscription) {
+					if (subscription) {
+						let rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+						const key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
+						let rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
+						const authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
+						const endpoint = subscription.endpoint;
+						const sub = {
+							endpoint: endpoint,
+							key: key,
+							authSecret: authSecret
+						};
+						Meteor.call("addWebPushSubscription", sub, function (error) {
+							if (error) {
+								throw new Meteor.Error(error.statusCode, 'Error subscription failed');
+							}
+						});
+					}
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
+
 Meteor.methods({
 	/**
 	 * Stores a web push subscription for the current user in the "webPushSubscriptions" collection.
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,7 +8,7 @@ import {Route} from "../../api/route.js";
 import {CardEditor} from "../../api/cardEditor";
 import {Filter} from "../../api/filter";
 import {MarkdeepEditor} from "../../api/markdeepEditor";
-import {CardsetNavigation} from "../../api/cardsetNavigation";
+import {WebPushNotifications} from "../../api/webPushSubscriptions";
 
 let loadingScreenTemplate = 'loadingScreen';
 
@@ -834,7 +834,7 @@ Router.route('/admin/settings', {
 	layoutTemplate: 'admin_main',
 	data: function () {
 		Session.set('helpFilter', undefined);
-		CardsetNavigation.subscribeForPushNotification();
+		WebPushNotifications.subscribeForPushNotification();
 	},
 	action: function () {
 		if (this.ready()) {
