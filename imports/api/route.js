ad380740d24a9800a96354ce4ad71b5d04111cca
Curtis Adam <curtis.adam@mni.thm.de>
2019-01-01 14:36:58 +0100

    Fix route name for profileOverview inside route.js

--- imports/api/route.js
+++ imports/api/route.js
@@ -66,7 +66,7 @@ export let Route = class Route {
 	}
 
 	static isLeitnerProgressProfileOverview () {
-		return Router.current().route.getName() === "summativeProgress";
+		return Router.current().route.getName() === "profileOverview";
 	}
 
 	/**
