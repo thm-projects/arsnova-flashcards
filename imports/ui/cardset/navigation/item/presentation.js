d9e31ab399f26d4f55b9818d73ae42ece4c12d6c
Curtis Adam <curtis.adam@mni.thm.de>
2019-01-12 20:07:02 +0100

    Always enable presentation mode and remove the unused contrast button setting

--- imports/ui/cardset/navigation/item/presentation.js
+++ imports/ui/cardset/navigation/item/presentation.js
@@ -1,6 +1,5 @@
 //------------------------ IMPORTS
 import {Template} from "meteor/templating";
-import {CardType} from "../../../../api/cardTypes";
 import "./presentation.html";
 
 /*
@@ -8,16 +7,6 @@ import "./presentation.html";
  * cardsetNavigationPresentation
  * ############################################################################
  */
-
-Template.cardsetNavigationPresentation.helpers({
-	gotPresentation: function () {
-		if (this.shuffled) {
-			return true;
-		} else {
-			return CardType.gotPresentationMode(this.cardType);
-		}
-	}
-});
 
 Template.cardsetNavigationPresentation.events({
 	"click #startPresentation": function () {
