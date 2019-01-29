a71ed0b700d82ad3a57a030df3d70fef5471aac4
Klaus-Dieter Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2019-01-29 10:45:24 +0100

    Update initialize.js

--- imports/startup/server/initialize.js
+++ imports/startup/server/initialize.js
@@ -13,7 +13,7 @@ import {Paid} from "../../api/paid";
 var initColorThemes = function () {
 	return [{
 		"_id": "default",
-		"name": " Kai Schreiber: »Zettelkasten«, CC BY-SA 2.0"
+		"name": "Kai Schreiber: »Zettelkasten«, CC BY-SA 2.0"
 	}, {
 		"_id": "contrast",
 		"name": "Ohne Hintergrundbild und mit mehr Kontrast"
