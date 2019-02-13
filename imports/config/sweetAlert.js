1bff52309d226e1f3d2562f34d5b95be8bcd1ca9
Curtis Adam <curtis.adam@mni.thm.de>
2019-02-13 09:23:02 +0100

    Update SweetAlert config

--- imports/config/sweetAlert.js
+++ imports/config/sweetAlert.js
@@ -1,47 +1,55 @@
-let continuePresentation = {
-	title: TAPi18n.__('sweetAlert.presentation.continue.title'),
-	html: TAPi18n.__('sweetAlert.presentation.continue.text'),
-	type: "warning",
-	showCancelButton: true,
-	confirmButtonText: TAPi18n.__('sweetAlert.presentation.continue.button.confirm'),
-	cancelButtonText: TAPi18n.__('sweetAlert.presentation.continue.button.cancel'),
-	allowOutsideClick: false,
-	footer: TAPi18n.__('sweetAlert.presentation.continue.footer')
-};
+function continuePresentation() {
+	return {
+		title: TAPi18n.__('sweetAlert.presentation.continue.title'),
+		html: TAPi18n.__('sweetAlert.presentation.continue.text'),
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonText: TAPi18n.__('sweetAlert.presentation.continue.button.confirm'),
+		cancelButtonText: TAPi18n.__('sweetAlert.presentation.continue.button.cancel'),
+		allowOutsideClick: false,
+		footer: TAPi18n.__('sweetAlert.presentation.continue.footer')
+	};
+}
 
-let completeProfile = {
-	title: TAPi18n.__('sweetAlert.completeProfile.title'),
-	html: TAPi18n.__('sweetAlert.completeProfile.text'),
-	type: "warning",
-	showCancelButton: true,
-	confirmButtonText: TAPi18n.__('sweetAlert.completeProfile.button.confirm'),
-	cancelButtonText: TAPi18n.__('sweetAlert.completeProfile.button.cancel'),
-	allowOutsideClick: false,
-	footer: TAPi18n.__('sweetAlert.completeProfile.footer')
-};
+function completeProfile() {
+	return {
+		title: TAPi18n.__('sweetAlert.completeProfile.title'),
+		html: TAPi18n.__('sweetAlert.completeProfile.text'),
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonText: TAPi18n.__('sweetAlert.completeProfile.button.confirm'),
+		cancelButtonText: TAPi18n.__('sweetAlert.completeProfile.button.cancel'),
+		allowOutsideClick: false,
+		footer: TAPi18n.__('sweetAlert.completeProfile.footer')
+	};
+}
 
-let activateFullscreen = {
-	title: TAPi18n.__('sweetAlert.fullscreen.title'),
-	html: TAPi18n.__('sweetAlert.fullscreen.text'),
-	type: "warning",
-	showCancelButton: true,
-	confirmButtonText: TAPi18n.__('sweetAlert.fullscreen.button.confirm'),
-	cancelButtonText: TAPi18n.__('sweetAlert.fullscreen.button.cancel'),
-	allowOutsideClick: false,
-	footer: TAPi18n.__('sweetAlert.fullscreen.footer')
-};
+function activateFullscreen() {
+	return {
+		title: TAPi18n.__('sweetAlert.fullscreen.title'),
+		html: TAPi18n.__('sweetAlert.fullscreen.text'),
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonText: TAPi18n.__('sweetAlert.fullscreen.button.confirm'),
+		cancelButtonText: TAPi18n.__('sweetAlert.fullscreen.button.cancel'),
+		allowOutsideClick: false,
+		footer: TAPi18n.__('sweetAlert.fullscreen.footer')
+	};
+}
 
 //Currently unused
-let exitPresentation = {
-	title: TAPi18n.__('sweetAlert.presentation.end.title'),
-	html: TAPi18n.__('sweetAlert.presentation.end.text'),
-	type: "warning",
-	showCancelButton: true,
-	confirmButtonText: TAPi18n.__('sweetAlert.presentation.end.button.confirm'),
-	cancelButtonText: TAPi18n.__('sweetAlert.presentation.end.button.cancel'),
-	allowOutsideClick: false,
-	footer: TAPi18n.__('sweetAlert.presentation.end.footer')
-};
+function exitPresentation() {
+	return {
+		title: TAPi18n.__('sweetAlert.presentation.end.title'),
+		html: TAPi18n.__('sweetAlert.presentation.end.text'),
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonText: TAPi18n.__('sweetAlert.presentation.end.button.confirm'),
+		cancelButtonText: TAPi18n.__('sweetAlert.presentation.end.button.cancel'),
+		allowOutsideClick: false,
+		footer: TAPi18n.__('sweetAlert.presentation.end.footer')
+	};
+}
 
 module.exports = {
 	continuePresentation,
