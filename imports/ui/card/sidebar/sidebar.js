58ef7728043aae33eb80733b778445a072120277
Curtis Adam <curtis.adam@mni.thm.de>
2019-03-06 12:35:38 +0100

    Take up as much of the card fullscreen as possible without breaking the carousel animation

--- imports/ui/card/sidebar/sidebar.js
+++ imports/ui/card/sidebar/sidebar.js
@@ -1,8 +1,6 @@
 //------------------------ IMPORTS
 
 import {Template} from "meteor/templating";
-import {Route} from "../../../api/route";
-import {CardNavigation} from "../../../api/cardNavigation";
 import {CardVisuals} from "../../../api/cardVisuals";
 import "./sidebar.html";
 import "./item/aspectRatio.js";
@@ -24,110 +22,23 @@ import "./item/help.js";
 import "./item/swapQuestionAnswer.js";
 import "./item/presentation.js";
 
-
-
 /*
  * ############################################################################
- * flashcardSidebar
+ * flashcardSidebarLeft
  * ############################################################################
  */
 
-Template.flashcardSidebar.helpers({
-	isCardset: function () {
-		return Route.isCardset();
-	},
-	isPresentation: function () {
-		return Route.isPresentation();
-	},
-	isMakingOf: function () {
-		return Route.isMakingOf();
-	},
-	isDemo: function () {
-		return Route.isDemo();
-	},
-	isEditMode: function () {
-		return Route.isEditMode();
-	},
-	isBox: function () {
-		return Route.isBox();
-	},
-	isMemo: function () {
-		return Route.isMemo();
-	},
-	isFixedSidebar: function () {
-		return CardVisuals.isFixedSidebar();
-	}
+Template.flashcardSidebarLeft.onRendered(function () {
+	CardVisuals.setSidebarPosition();
 });
 
 /*
  * ############################################################################
- * flashcardSidebarDefaultLeft
+ * flashcardSidebarRight
  * ############################################################################
  */
 
-Template.flashcardSidebarDefaultLeft.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
-});
 
-
-/*
- * ############################################################################
- * flashcardSidebarDefaultRight
- * ############################################################################
- */
-
-Template.flashcardSidebarDefaultRight.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
-});
-
-/*
- * ############################################################################
- * flashcardSidebarPresentationLeft
- * ############################################################################
- */
-
-Template.flashcardSidebarPresentationLeft.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
-});
-
-/*
- * ############################################################################
- * flashcardSidebarPresentationRight
- * ############################################################################
- */
-
-Template.flashcardSidebarPresentationRight.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
-});
-
-/*
- * ############################################################################
- * flashcardSidebarDemoLeft
- * ############################################################################
- */
-
-Template.flashcardSidebarDemoLeft.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
-});
-
-/*
- * ############################################################################
- * flashcardSidebarDemoRight
- * ############################################################################
- */
-
-Template.flashcardSidebarDemoRight.helpers({
-	isMobileView: function () {
-		return CardNavigation.isMobileView();
-	}
+Template.flashcardSidebarRight.onRendered(function () {
+	CardVisuals.setSidebarPosition();
 });
