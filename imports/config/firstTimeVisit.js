94934a269f082ddb5a200f89b451e3e48ed7ddd7
Curtis Adam <curtis.adam@mni.thm.de>
2019-03-20 13:46:08 +0100

    Add an option to disable the first time visit demo redirect

--- imports/config/firstTimeVisit.js
+++ imports/config/firstTimeVisit.js
@@ -1,8 +1,11 @@
 //Settings for first time visitors
+let enableFirstTimeVisitDemo = false;
 let enableFirstTimeVisitModal = false;
 let redirectToHomeAfterExit = true;
 let enableFirstTimePresentationModal = false;
+
 module.exports = {
+	enableFirstTimeVisitDemo,
 	enableFirstTimeVisitModal,
 	redirectToHomeAfterExit,
 	enableFirstTimePresentationModal
