8ce0c18f954fda44b7c9941fde0e48d11457b326
Curtis Adam <curtis.adam@mni.thm.de>
2019-05-13 15:23:55 +0200

    Transcript bonus info: Add Maximum bonus points

--- imports/ui/cardset/info/box/bonusTranscript.js
+++ imports/ui/cardset/info/box/bonusTranscript.js
@@ -2,6 +2,7 @@
 import {Template} from "meteor/templating";
 import {CardsetVisuals} from "../../../../api/cardsetVisuals";
 import "./item/bonusPointsFAQ.js";
+import "./item/transcript/bonusMaxPoints.js";
 import "./item/transcript/editDeadline.js";
 import "./item/transcript/lectureDates.js";
 import "./item/transcript/submissionDeadline.js";
