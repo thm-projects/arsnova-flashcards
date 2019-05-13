4e35467d570f81efa3263f9396ea3851031bff49
Curtis Adam <curtis.adam@mni.thm.de>
2019-05-13 13:27:25 +0200

    Transcript-Bonus info: Display the submission and editing deadlines

--- imports/ui/cardset/info/box/bonusTranscript.js
+++ imports/ui/cardset/info/box/bonusTranscript.js
@@ -1,19 +1,9 @@
 //------------------------ IMPORTS
 import {Template} from "meteor/templating";
 import {CardsetVisuals} from "../../../../api/cardsetVisuals";
-import "./item/bonusActiveLearners.js";
-import "./item/bonusButton.js";
-import "./item/bonusDeadline.js";
-import "./item/bonusEnd.js";
-import "./item/bonusMaxPoints.js";
 import "./item/bonusPointsFAQ.js";
-import "./item/bonusRegistrationPeriod.js";
-import "./item/bonusStart.js";
-import "./item/bonusStatus.js";
-import "./item/bonusWorkload.js";
-import "./item/pomodoroBreakTime.js";
-import "./item/pomodoroCount.js";
-import "./item/pomodoroWorkTime.js";
+import "./item/transcript/editDeadline.js";
+import "./item/transcript/submissionDeadline.js";
 import "./bonusTranscript.html";
 
 /*
