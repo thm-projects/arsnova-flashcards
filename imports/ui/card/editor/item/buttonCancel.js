0b0c43a95dbaa4cc8e768ea6ea030b7ea2633276
Curtis Adam <curtis.adam@mni.thm.de>
2019-05-20 16:06:20 +0200

    Disable the cancel card editing modal

--- imports/ui/card/editor/item/buttonCancel.js
+++ imports/ui/card/editor/item/buttonCancel.js
@@ -1,1 +1,33 @@
 import "./buttonCancel.html";
+import {Template} from "meteor/templating";
+import {Route} from "../../../../api/route";
+import {Session} from "meteor/session";
+
+/*
+ * ############################################################################
+ * cardEditorItemButtonCancel
+ * ############################################################################
+ */
+
+Template.cardEditorItemButtonCancel.events({
+	'click #cardCancel': function () {
+		if (Route.isTranscript()) {
+			if (Session.get('transcriptBonus') !== undefined) {
+				Router.go('transcriptsBonus');
+			} else {
+				Router.go('transcriptsPersonal');
+			}
+		} else {
+			Session.set('activeCard', Router.current().params.card_id);
+			if (Route.isNewCard()) {
+				Router.go('cardsetdetailsid', {
+					_id: Router.current().params._id
+				});
+			} else {
+				Router.go('presentation', {
+					_id: Router.current().params._id
+				});
+			}
+		}
+	}
+});
