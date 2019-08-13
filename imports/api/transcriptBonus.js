172e70bf5ae690efee4506c82357359f30b61722
Curtis Adam <curtis.adam@mni.thm.de>
2019-08-13 16:42:43 +0200

    Force the stars median as a comma separated value

--- imports/api/transcriptBonus.js
+++ imports/api/transcriptBonus.js
@@ -183,7 +183,7 @@ export let TranscriptBonusList = class TranscriptBonusList {
 			if (config.roundTheStarsMedian) {
 				return Math.round(median);
 			} else {
-				return +(median).toFixed(2);
+				return (median).toFixed(2).toString().replace('.', ',');
 			}
 		}
 	}
