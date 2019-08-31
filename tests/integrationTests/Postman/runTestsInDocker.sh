489ebb22388f1ede4d1cecb908ee222b9e2b81a4
Yannik Hoehnel <yannik.hoehnel@gmail.com>
2019-08-31 14:43:06 +0200

    add shell script to run integration tests inside a docker image

---
+++ tests/integrationTests/Postman/runTestsInDocker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# first pulls the needed docker image
+# then run the integration tests in the docker container
+
+if [ "$1" = "" ]
+then
+	echo "Parameter 1 needed: location of postman collection"
+	exit 1
+fi
+
+docker pull postman/newman:ubuntu
+
+docker run --network="host" -v $1:/etc/newman -t postman/newman:ubuntu run "SystematischerSoftwaretest.postman_collection.json" -e "SystematischerSoftwareTestEnv.postman_environment.json"
