154f171ea8277fa5b4344b04550eec79ec36adfb
Yannik Hoehnel <yannik.hoehnel@gmail.com>
2019-09-04 14:31:49 +0200

    add first unittests for card creating

--- imports/api/cards.test.js
+++ imports/api/cards.test.js
@@ -1,12 +1,58 @@
-import '../../imports/api/cards.js';
-import {Meteor} from "meteor/meteor";
-import {assert} from "chai";
+import './cards.js';
+import { Meteor } from "meteor/meteor";
+import { assert, expect } from "chai";
 
-if (Meteor.isServer) {
-	describe('addCard', function () {
-					it('can create a new card', function () {
-									assert.equal(1, 1);
-								});
-				});
-}
+
+describe('addCard', function () {
+
+	beforeEach(() => {
+		DDP._CurrentInvocation.get = function () {
+			return {
+				userId: 'admin'
+			};
+		};
+	});
+
+
+	it('can create a new card', function () {
+		let cardset_id = "-1";
+		let subject = 'Aktivitätsdiagramm';
+		let content1 = 'Test content 1';
+		let content2 = 'Test content 2';
+		let content3 = 'Test content 3';
+		let content4 = 'Test content 4';
+		let content5 = 'Test content 5';
+		let content6 = 'Test content 6';
+		let centerTextElement = [false, false, false, false];
+		let alignType = [0, 0, 0, 0, 0, 0];
+		let date = new Date();
+		let learningGoalLevel = 0;
+		let backgroundStyle = 0;
+		let bonusUser = false;
+
+		let cardId = Meteor.call('addCard', cardset_id, subject, content1, content2, content3, content4, content5, content6, centerTextElement, alignType, date, learningGoalLevel, backgroundStyle, bonusUser);
+		assert.isAbove(cardId.length, 0);
+	});
+
+	it('should fail without subject', function () {
+		expect(function () {
+			let cardset_id = "-1";
+			let subject = '';
+			let content1 = 'Test content 1';
+			let content2 = 'Test content 2';
+			let content3 = 'Test content 3';
+			let content4 = 'Test content 4';
+			let content5 = 'Test content 5';
+			let content6 = 'Test content 6';
+			let centerTextElement = [false, false, false, false];
+			let alignType = [0, 0, 0, 0, 0, 0];
+			let date = new Date();
+			let learningGoalLevel = 0;
+			let backgroundStyle = 0;
+
+			Meteor.call('addCard', cardset_id, subject, content1, content2, content3, content4, content5, content6, centerTextElement, alignType, date, learningGoalLevel, backgroundStyle);
+
+		}).to.throw();
+	});
+});
 
