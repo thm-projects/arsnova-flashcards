81ef861acd468d3202ad9d1a576ba8758d15b5ad
Yannik Hoehnel <yannik.hoehnel@gmail.com>
2019-09-05 13:04:55 +0200

    edit shell script

--- tests/unitTests/start_unit_tests.sh
+++ tests/unitTests/start_unit_tests.sh
@@ -2,6 +2,6 @@
 
 cd ../../
 
-BABEL_ENV=COVERAGE COVERAGE=1 COVERAGE_OUT_HTML=1 COVERAGE_APP_FOLDER=$PWD/tests  meteor test --driver-package meteortesting:mocha --once
+BABEL_ENV=COVERAGE COVERAGE=1 COVERAGE_OUT_HTML=1 COVERAGE_APP_FOLDER=$PWD/  meteor test --driver-package meteortesting:mocha --once
 
 cd tests/unitTests
