ec5d68ea23eb80211cf9f03735e92359397f72f8
Yannik Hoehnel <yannik.hoehnel@gmail.com>
2019-09-10 18:05:44 +0200

    add error output for unit test script

--- tests/unitTests/start_unit_tests.sh
+++ tests/unitTests/start_unit_tests.sh
@@ -2,6 +2,7 @@
 
 cd ../../
 
-BABEL_ENV=COVERAGE COVERAGE=1 COVERAGE_OUT_LCOVONLY=1 COVERAGE_APP_FOLDER=$PWD/  meteor test --driver-package meteortesting:mocha --once
+set -e
+BABEL_ENV=COVERAGE COVERAGE=1 COVERAGE_OUT_LCOVONLY=1 COVERAGE_OUT_HTML=1 COVERAGE_APP_FOLDER=$PWD/ meteor test --driver-package meteortesting:mocha --once
 
 cd tests/unitTests
