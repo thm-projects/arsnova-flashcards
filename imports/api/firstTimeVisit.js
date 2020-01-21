b25b5a1e465c49fee397776dd7e5449e1da123e6
Curtis Adam <curtis.adam@mni.thm.de>
2020-01-21 12:46:42 +0100

    Remove broken and unused demo landing page modal

--- imports/api/firstTimeVisit.js
+++ imports/api/firstTimeVisit.js
@@ -2,10 +2,6 @@ import * as conf from "../config/firstTimeVisit.js";
 
 export let FirstTimeVisit = class FirstTimeVisit {
 
-	static isFirstTimeVisitModalEnabled () {
-		return conf.enableFirstTimeVisitModal;
-	}
-
 	static redirectToHomeAfterFullscreenExit () {
 		return conf.redirectToHomeAfterExit;
 	}
--- imports/config/firstTimeVisit.js
+++ imports/config/firstTimeVisit.js
@@ -1,12 +1,10 @@
 //Settings for first time visitors
 let enableFirstTimeVisitDemo = false;
-let enableFirstTimeVisitModal = false;
 let redirectToHomeAfterExit = true;
 let enableFirstTimePresentationModal = false;
 
 module.exports = {
 	enableFirstTimeVisitDemo,
-	enableFirstTimeVisitModal,
 	redirectToHomeAfterExit,
 	enableFirstTimePresentationModal
 };
