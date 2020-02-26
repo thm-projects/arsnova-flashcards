517b6bf5bfc2ba1dc801a8d2f68e91fa62d9a9b4
Nils Mittler <nils.mittler@mni.thm.de>
2020-02-26 12:45:20 +0100

    produce ready-to-use image

--- .docker/kaniko/entrypoint.sh
+++ .docker/kaniko/entrypoint.sh
@@ -2,6 +2,6 @@
 
 export NODE_ENV=production
 export PORT=3000
-export METEOR_SETTINGS="`cat settings_docker.json`"
+export METEOR_SETTINGS="`cat settings_kaniko.json`"
 
 node /usr/app/bundle/main.js
