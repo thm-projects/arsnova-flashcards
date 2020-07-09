ccd52b7e426bd7afa6a678a8cc5458bf7de6c4a8
Curtis Adam <curtis.adam@mni.thm.de>
2020-07-09 13:08:20 +0200

    Convert background image config to objects

--- imports/config/backgrounds.js
+++ imports/config/backgrounds.js
@@ -2,83 +2,219 @@
 // - Set the Background-Colors inside the Theme Switcher files.
 // - Set the Background-Image to "none" to enable the Background-Color.
 // - Change the setting "backgrounds" in "/import/config/server.js" to select the background setting that the server should use.
+// - Add additional attributes to the object if needed. For example: "background-size": "contain"
+
 
 // Setting "arsnova"
 let arsnova = {
-	"landing-page": "/img/background/question-mark-1481601.jpg",
-	"demo": "/img/background/question-mark-1481601.jpg", // Defaults to landing-page if off
-	"demoIndex": "/img/background/question-mark-1481601.jpg", // Defaults to landing-page if off
-	"about": "none", // Defaults to landing-page if off
-	"learning": "none", // Defaults to landing-page if off
-	"faq": "none", // Defaults to landing-page if off
-	"help": "none", // Defaults to landing-page if off
-	"impressum": "none", // Defaults to landing-page if off
-	"agb": "none", // Defaults to landing-page if off
-	"datenschutz": "none", // Defaults to landing-page if off
-	"internal": "/img/background/question-mark-1481601.jpg",
-	"pool": "none", // Defaults to internal if off
-	"workload": "none", // Defaults to internal if off
-	"personal": "none", // Defaults to internal if off
-	"transcripts": "/img/background/pencil.png", // Defaults to internal if off
-	"allPool": "none", // Defaults to internal if off
-	"cardset": "none", // Defaults to internal if off
-	"cardsetLeitnerStats": "none", // Defaults to cardset if off
-	"cardsetTranscriptBonus": "none", // Defaults to cardset if off
-	"leitner": "none", // Defaults to internal if off
-	"wozniak": "none", // Defaults to internal if off
-	"presentation": "none", // Defaults to internal if off
-	"presentationIndex": "none", // Defaults to internal if off
-	"editor": "/img/background/matrix.jpg", // Defaults to internal if off
-	"profileMembership": "none", // Defaults to internal if off
-	"profileBilling": "none", // Defaults to internal if off
-	"profileSettings": "none", // Defaults to internal if off
-	"profileRequests": "none", // Defaults to internal if off
-	"backend": "/img/background/matrix.jpg"
+	"landing-page": {
+		"background-image": "url('/img/background/question-mark-1481601.jpg')"
+	},
+	"demo": {
+		"background-image": "url('/img/background/question-mark-1481601.jpg')"
+	}, // Defaults to landing-page if off
+	"demoIndex": {
+		"background-image": "url('/img/background/question-mark-1481601.jpg')"
+	}, // Defaults to landing-page if off
+	"about": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"learning": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"faq": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"help": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"impressum": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"agb": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"datenschutz": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"internal": {
+		"background-image": "url('/img/background/question-mark-1481601.jpg')"
+	},
+	"pool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"workload": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"personal": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"transcripts": {
+		"background-image": "url('/img/background/pencil.png')",
+		"background-size": "contain"
+	}, // Defaults to internal if off
+	"allPool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardset": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardsetLeitnerStats": {
+		"background-image": "none"
+	}, // Defaults to cardset if off
+	"cardsetTranscriptBonus": {
+		"background-image": "none"
+	}, // Defaults to cardset if off
+	"leitner": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"wozniak": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentation": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentationIndex": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"editor": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}, // Defaults to internal if off
+	"profileMembership": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileBilling": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileSettings": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileRequests": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"backend": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}
 };
 
 // Setting "linux"
 let linux = {
-	"landing-page": "/img/background/Linux-Regal.jpg",
-	"demo": "/img/background/matrix.jpg",
-	"demoIndex": "/img/background/matrix.jpg",
-	"about": "none", // Defaults to landing-page if off
-	"learning": "none", // Defaults to landing-page if off
-	"faq": "none", // Defaults to landing-page if off
-	"help": "none", // Defaults to landing-page if off
-	"impressum": "none", // Defaults to landing-page if off
-	"agb": "none", // Defaults to landing-page if off
-	"datenschutz": "none", // Defaults to landing-page if off
-	"internal": "/img/background/matrix.jpg",
-	"pool": "none", // Defaults to internal if off
-	"workload": "none", // Defaults to internal if off
-	"personal": "none", // Defaults to internal if off
-	"transcripts": "none", // Defaults to internal if off
-	"allPool": "none", // Defaults to internal if off
-	"cardset": "none", // Defaults to internal if off
-	"cardsetLeitnerStats": "none", // Defaults to cardset if off
-	"cardsetTranscriptBonus": "/img/background/pencil.png", // Defaults to cardset if off
-	"leitner": "none", // Defaults to internal if off
-	"wozniak": "none", // Defaults to internal if off
-	"presentation": "none", // Defaults to internal if off
-	"presentationIndex": "none", // Defaults to internal if off
-	"editor": "/img/background/matrix.jpg", // Defaults to internal if off
-	"profileMembership": "none", // Defaults to internal if off
-	"profileBilling": "none", // Defaults to internal if off
-	"profileSettings": "none", // Defaults to internal if off
-	"profileRequests": "none", // Defaults to internal if off
-	"backend": "/img/background/matrix.jpg"
+	"landing-page": {
+		"background-image": "url('/img/background/Linux-Regal.jpg')"
+	},
+	"demo": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"demoIndex": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"about": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"learning": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"faq": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"help": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"impressum": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"agb": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"datenschutz": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"internal": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"pool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"workload": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"personal": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"transcripts": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"allPool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardset": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardsetLeitnerStats": {
+		"background-image": "none"
+	}, // Defaults to cardset if off
+	"cardsetTranscriptBonus": {
+		"background-image": "url('/img/background/pencil.png')",
+		"background-size": "contain"
+	}, // Defaults to cardset if off
+	"leitner": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"wozniak": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentation": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentationIndex": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"editor": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}, // Defaults to internal if off
+	"profileMembership": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileBilling": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileSettings": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileRequests": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"backend": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}
 };
 
 // Setting disabled
 let disabled = {
-	"landing-page": "none",
-	"internal": "none",
-	"demo": "none",
-	"presentation": "none",
-	"learning": "none",
-	"backend": "none",
-	"editor": "none",
-	"transcriptBonus": "none"
+	"landing-page": {
+		"background-image": "none"
+	},
+	"internal": {
+		"background-image": "none"
+	},
+	"demo": {
+		"background-image": "none"
+	},
+	"presentation": {
+		"background-image": "none"
+	},
+	"learning": {
+		"background-image": "none"
+	},
+	"backend": {
+		"background-image": "none"
+	},
+	"editor": {
+		"background-image": "none"
+	},
+	"transcriptBonus": {
+		"background-image": "none"
+	}
 };
 
 module.exports = {
