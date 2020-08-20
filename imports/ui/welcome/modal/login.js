7d48bca948a4ce542338e2c37a38d4dbbca47884
Curtis Adam <curtis.adam@mni.thm.de>
2020-08-20 16:32:27 +0200

    Add an event to trigger a redirect on password reset

--- imports/ui/welcome/modal/login.js
+++ imports/ui/welcome/modal/login.js
@@ -1,6 +1,7 @@
 import "./login.html";
 import "../../../util/accounts.js";
 import {Template} from "meteor/templating";
+import {LoginTasks} from "../../../util/login";
 
 Template.loginModal.onCreated(function () {
 	T9n.setLanguage('de');
@@ -14,6 +15,10 @@ Template.loginModal.onRendered(function () {
 
 Template.loginModal.onDestroyed(function () {
 	$(".modal-backdrop").remove();
+});
+
+Template.redirectPasswordReset.onRendered(function () {
+	LoginTasks.setLoginRedirect();
 });
 
 Template['cards-login'].replaces('atForm');
