4108f88988a9abbc7b72b05bca311115cfe23c87
Loic Cesar Boubeji Tiangueu <loic.cesar.boubeji.tiangueu@mni.thm.de>
2021-03-03 21:00:58 +0000

    Add tooltips for days before reset and intervals in bonus modal
    (cherry picked from commit 6b3c9f66733ab413170b3fbaecd8dbcc51a3349e)

--- imports/ui/cardset/navigation/modal/bonus/item/daysBeforeReset.js
+++ imports/ui/cardset/navigation/modal/bonus/item/daysBeforeReset.js
@@ -1,11 +1,24 @@
 import "./daysBeforeReset.html";
+import "../tooltip/de/daysBeforeReset.html";
+import "../tooltip/en/daysBeforeReset.html";
 import {BonusForm} from "../../../../../../util/bonusForm";
+import tippy from "tippy.js";
 
 /*
 * ############################################################################
 * bonusFormDaysBeforeReset
 * ############################################################################
 */
+
+Template.bonusFormDaysBeforeReset.onRendered(function () {
+	const template = document.getElementById('bonus-reset-tooltip');
+
+	tippy('#bonus-reset-info', {
+		content: template.innerHTML,
+		allowHTML: true
+	});
+});
+
 
 Template.bonusFormDaysBeforeReset.events({
 	"input #daysBeforeReset": function () {
--- imports/ui/cardset/navigation/modal/bonus/item/intervals.js
+++ imports/ui/cardset/navigation/modal/bonus/item/intervals.js
@@ -1,12 +1,27 @@
 import "./intervals.html";
+import "../tooltip/de/intervals.html";
+import "../tooltip/en/intervals.html";
 import {BonusForm} from "../../../../../../util/bonusForm";
 import {LearningStatus} from "../../../../../../util/learningStatus";
+import tippy from "tippy.js";
 
 /*
 * ############################################################################
 * bonusFormIntervals
 * ############################################################################
 */
+
+
+Template.bonusFormIntervals.onRendered(function () {
+	const template = document.getElementById('bonus-intervals-tooltip');
+
+	tippy('#bonus-intervals-info', {
+		content: template.innerHTML,
+		allowHTML: true
+	});
+});
+
+
 
 Template.bonusFormIntervals.events({
 	"change input": function () {
