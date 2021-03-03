7e4f9f4fcfe93dc5bc3033bf424f214e1ea92805
Curtis Adam <curtis.adam@mni.thm.de>
2021-03-03 15:48:12 +0100

    Update Test-Database scripts

--- tests/loadTestDatabase.sh
+++ tests/loadTestDatabase.sh
@@ -2,18 +2,34 @@
 
 DIRECTORY=`dirname $0`
 CALLED_FROM_SCRIPT=true
-
+URL='https://arsnova-uploads.mni.thm.de/cards/debug.cards-database.gz'
+STATUS=0
+FILE='./tests/testDatabaseDump/debug.cards-database.gz'
 source $DIRECTORY/helpers/scriptSettings.sh
 
 checkDirectory
 
-# Drop and Restore the Test-Database
-echo -e $GREEN"\nDropping and Restoring Test-Database ..."$NC
-if ! mongorestore --quiet --drop -h "$MONGO_HOST" --port "$MONGO_PORT" -d "$MONGO_DB" $restoreDir 1> /dev/null; then
-	echo -e $RED"mongorestore failed!"$NC
-	exit 2
+
+# Checking version of Test-Database dump
+if curl --output /dev/null --silent --head -faill "$URL"; then
+  echo -e $GREEN"Checking online version..."$NC
+  curl -z "$FILE" "$URL" --output "$FILE"
+else
+  echo -e $RED"Can't find online Test-Database."$NC
 fi
-echo -e $GREEN"Test-Database restored."$NC
-if [ -z "$CALLED_FROM_SCRIPT" ] ; then
-    exit 0
+
+if test -f "$FILE"; then
+  # Drop and Restore the Test-Database
+  echo -e $GREEN"Dropping and Restoring Test-Database ..."$NC
+  if ! mongorestore --drop -h "$MONGO_HOST" --port "$MONGO_PORT" -d "$MONGO_DB" --gzip --archive="./tests/testDatabaseDump/debug.cards-database.gz" --nsFrom "cards.*" --nsTo "cards.*" 1> /dev/null; then
+    echo -e $RED"mongorestore failed!"$NC
+    exit 2
+  fi
+  echo -e $GREEN"Test-Database restored."$NC
+  if [ -z "$CALLED_FROM_SCRIPT" ] ; then
+      exit 0
+  fi
+else
+  echo -e $RED"Can't find Test-Database."$NC
 fi
+
