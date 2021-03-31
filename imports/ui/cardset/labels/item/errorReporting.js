b04a2b7aabef69fa0ebd20c195d824ff5cbe70dc
Curtis Adam <curtis.adam@mni.thm.de>
2021-03-31 14:40:16 +0200

    Fix error count not displaying on every route if used inside the use case modal

--- imports/ui/cardset/labels/item/errorReporting.js
+++ imports/ui/cardset/labels/item/errorReporting.js
@@ -3,20 +3,21 @@ import {Template} from "meteor/templating";
 import {Session} from "meteor/session";
 import {FlowRouter} from "meteor/ostrio:flow-router-extra";
 import * as RouteNames from "../../../../util/routeNames";
-import {ErrorReporting} from "../../../../util/errorReporting";
 
 Template.cardsetLabelsItemErrorReporting.events({
 	'click .show-error': function (event) {
 		Session.set('showOnlyErrorReports', true);
+		$('#useCasesModal').modal('hide');
 		FlowRouter.go(RouteNames.presentationlist, {_id: $(event.target).data('id')});
 	}
 });
 
 Template.cardsetLabelsItemErrorReporting.helpers({
-	hasCardsetUnresolvedErrors: function (cardset_id) {
-		return ErrorReporting.getErrorCountFromCardset(cardset_id) > 0;
-	},
-	getErrorCountFromCardset: function (cardset_id) {
-		return ErrorReporting.getErrorCountFromCardset(cardset_id);
+	cardsetErrorCount: function () {
+		if (this.unresolvedErrors === undefined) {
+			return 0;
+		} else {
+			return this.unresolvedErrors;
+		}
 	}
 });
