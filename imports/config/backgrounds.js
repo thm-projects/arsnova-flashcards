b44da41718feb69d757585ba82549616e27363c9
Flo Dörr <flo@flodoerr.com>
2021-04-22 16:24:07 +0200

    :fire: Add new dark theme

--- imports/config/backgrounds.js
+++ imports/config/backgrounds.js
@@ -228,8 +228,106 @@ let disabled = {
 	}
 };
 
+// Setting "linux"
+let dark = {
+	"landing-page": {
+		"background-image": "url('/img/background/Linux-Regal.jpg')"
+	},
+	"demo": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"demoIndex": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"about": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"learning": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"faq": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"help": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"impressum": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"agb": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"datenschutz": {
+		"background-image": "none"
+	}, // Defaults to landing-page if off
+	"internal": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"pool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"workload": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"personal": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"transcripts": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"allPool": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardset": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"cardsetLeitnerStats": {
+		"background-image": "none"
+	}, // Defaults to cardset if off
+	"cardsetTranscriptBonus": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}, // Defaults to cardset if off
+	"leitner": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"wozniak": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentation": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"presentationIndex": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"editor": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	}, // Defaults to internal if off
+	"profileMembership": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileBilling": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileSettings": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"profileRequests": {
+		"background-image": "none"
+	}, // Defaults to internal if off
+	"notFound": {
+		"background-image": "none"
+	}, // Defaults to internal if logged in, otherwise to landing-page
+	"backend": {
+		"background-image": "url('/img/background/matrix.jpg')"
+	},
+	"wordcloud": {
+		"background-image": "url('/img/background/Night.jpg')"
+	}
+};
+
 module.exports = {
 	arsnova,
 	linux,
+	dark,
 	disabled
 };
