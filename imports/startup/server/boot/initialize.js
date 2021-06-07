765c41eeefa59c18e963be18e26ad38900c1fe3d
Curtis Adam <curtis.adam@mni.thm.de>
2021-06-07 10:32:39 +0200

    Remove legacy leitner migration steps

--- imports/startup/server/boot/initialize.js
+++ imports/startup/server/boot/initialize.js
@@ -6,10 +6,6 @@ import {adminSettingsStep} from "./steps/adminSettingsStep";
 import {userMigrationStep} from "./steps/migration/userMigration";
 import {cardMigrationStep} from "./steps/migration/cardMigration";
 import {cardsetMigrationStep} from "./steps/migration/cardsetMigration";
-import {leitnerMigrationStep} from "./steps/migration/learningStatistics/legacyLeitner/leitnerMigration";
-import {leitnerHistoryMigrationStep} from "./steps/migration/learningStatistics/legacyLeitner/leitnerHistoryMigration";
-import {leitnerTaskMigrationStep} from "./steps/migration/learningStatistics/legacyLeitner/leitnerTaskMigration";
-import {workloadMigrationStep} from "./steps/migration/learningStatistics/legacyLeitner/workloadMigration";
 import {wozniakMigrationStep} from "./steps/migration/wozniakMigration";
 import {transcriptBonusMigrationStep} from "./steps/migration/transcriptBonusMigration";
 import {leitnerLearningWorkload} from "./steps/migration/learningStatistics/leitner/leitnerLearningWorkload";
@@ -28,10 +24,6 @@ Meteor.startup(function () {
 	userMigrationStep();
 	cardMigrationStep();
 	cardsetMigrationStep();
-	leitnerMigrationStep();
-	leitnerHistoryMigrationStep();
-	leitnerTaskMigrationStep();
-	workloadMigrationStep();
 	wozniakMigrationStep();
 	transcriptBonusMigrationStep();
 
