dc33598dd30b24e0eae38ca471ff0ca88bac6229
Flo Dörr <flo@flodoerr.com>
2021-06-18 15:25:08 +0200

    :wrench: add content to notification dialog

--- imports/ui/main/navigation/top/item/notification/frontendNotification.js
+++ imports/ui/main/navigation/top/item/notification/frontendNotification.js
@@ -2,8 +2,30 @@ import './frontendNotification.html';
 import {MessageOfTheDay} from "../../../../../../api/subscriptions/messageOfTheDay";
 import {openMessage} from "../../../../../messageOfTheDay/messageOfTheDay";
 import {Mongo} from "meteor/mongo";
+import { Cards } from '../../../../../../api/subscriptions/cards';
 
 const OwnErrorReportings = new Mongo.Collection("errorReporting");
+
+function getErrorMessage(reporting) {
+	let errors = '<ul>';
+	console.log(reporting);
+	if (reporting.error.type.includes(0)) {
+		errors += `<li>${TAPi18n.__('modal-card.errorReporting.spellingMistake')}</li>`;
+	}
+	if (reporting.error.type.includes(1)) {
+		errors += `<li>${TAPi18n.__('modal-card.errorReporting.missingPicture')}</li>`;
+	}
+	if (reporting.error.type.includes(2)) {
+		errors += `<li>${TAPi18n.__('modal-card.errorReporting.layoutMistake')}</li>`;
+	}
+	if (reporting.error.type.includes(3)) {
+		errors += `<li>${TAPi18n.__('modal-card.errorReporting.brokenLink')}</li>`;
+	}
+	if (reporting.error.content.length) {
+		errors += `<li>${TAPi18n.__('modal-card.errorReporting.otherError')}:<br />${reporting.error.content}</li>`;
+	}
+	return errors;
+}
 
 Template.frontendNotification.helpers({
 	getMessages: function () {
@@ -13,7 +35,11 @@ Template.frontendNotification.helpers({
 				{expirationDate: {$gte: new Date()}}
 			]}).fetch();
 		for (const reporting of OwnErrorReportings.find({}).fetch()) {
-			messages.push({subject: `Error reporting: ${reporting.error.content === "" ? reporting.error.type : reporting.error.content}`});
+			messages.push({subject: TAPi18n.__('modal-card.errorReporting.errorNotificationHeader') + Cards.findOne({_id: reporting.card_id}).subject, content: `<div>
+			${TAPi18n.__('modal-card.errorReporting.reportedErrorsNotification')}
+			${getErrorMessage(reporting)}
+			<a target="_self" rel="noopener noreferrer" href="/cardset/${reporting.cardset_id}">${TAPi18n.__('modal-card.errorReporting.openCardNotification')}</a>
+			</div>`});
 		}
 		if (messages.length === 0) {
 			$('#fallbackTR').show();
