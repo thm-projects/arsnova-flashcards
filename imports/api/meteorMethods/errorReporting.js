f1f6efaf6c46ccda06f61f5b2b3b97c98d08adff
Flo Dörr <flo@flodoerr.com>
2021-06-23 16:29:36 +0200

    :wrench: finally fix error

--- imports/api/meteorMethods/errorReporting.js
+++ imports/api/meteorMethods/errorReporting.js
@@ -19,17 +19,17 @@ Meteor.methods({
 
 		if (Meteor.isServer) {
 			ErrorReporting.insert({
-					user_id: user_id,
-					cardset_id: cardset_id,
-					card_id: card_id,
-					createdAt: new Date(),
-					status: 0,
-					cardSide: cardSide,
-					error: {
-						type: errorTypes,
-						content: errorContent
-					}
+				user_id: user_id,
+				cardset_id: cardset_id,
+				card_id: card_id,
+				createdAt: new Date(),
+				status: 0,
+				cardSide: cardSide,
+				error: {
+					type: errorTypes,
+					content: errorContent
 				}
+			}
 			);
 			Meteor.call("updateUnresolvedErrors", cardset_id, card_id);
 		}
@@ -43,16 +43,17 @@ Meteor.methods({
 		check(errorContent, String);
 		if (Meteor.isServer) {
 			ErrorReporting.update({_id: error_id},
-				{$set: {
-					updatedAt: new Date(),
-					status: 0,
-					cardSide: cardSide,
-					error: {
-						type: errorTypes,
-						content: errorContent
+				{
+					$set: {
+						updatedAt: new Date(),
+						status: 0,
+						cardSide: cardSide,
+						error: {
+							type: errorTypes,
+							content: errorContent
+						}
 					}
-				}
-			});
+				});
 			Meteor.call("updateUnresolvedErrors", cardset_id, card_id);
 		}
 	},
@@ -63,27 +64,54 @@ Meteor.methods({
 		check(status, Number);
 		if (Meteor.isServer) {
 			ErrorReporting.update({_id: error_id},
-				{$set: {
-					updatedAt: new Date(),
-					status: status
-				}
-			});
+				{
+					$set: {
+						updatedAt: new Date(),
+						status: status
+					}
+				});
 			Meteor.call("updateUnresolvedErrors", cardset_id, card_id);
 		}
 	},
-	updateUnresolvedErrors: function (cardset_id, card_id) {
+	updateUnresolvedErrors: function (cardset_id, card_id, top_cardset) {
 		if (Meteor.isServer) {
-			let countCardset = ErrorReporting.find({cardset_id: cardset_id, status: 0}).count();
-			Cardsets.update({_id: cardset_id}, {$set: {unresolvedErrors: countCardset}});
-			let countCard = ErrorReporting.find({card_id: card_id, status: 0}).count();
-			Cards.update({_id: card_id}, {$set: {unresolvedErrors: countCard}});
+			const cardset_ids = [cardset_id];
+			const cardsets = Cardsets.find({cardGroups: {$all: [cardset_id]}}).fetch();
+			for (const set of cardsets) {
+				if (set._id !== cardset_id) {
+					cardset_ids.push(set._id);
+				}
+			}
+			const countCardsets = (Cardsets.findOne({_id: cardset_id}).cardGroups || []).concat(cardset_ids);
+			if (card_id !== "") {
+				const realCardset = Cards.findOne({_id: card_id}).cardset_id;
+				if (realCardset && realCardset !== cardset_id) {
+					cardset_ids.push(realCardset);
+				}
+			}
+
+			if (top_cardset) {
+				cardset_ids.splice(cardset_ids.indexOf(top_cardset), 1);
+			}
+			const countCardset = ErrorReporting.find({cardset_id: {$in: countCardsets}, status: 0}).count();
+			for (const id of cardset_ids) {
+				if (id === cardset_id) {
+					Cardsets.update({_id: id}, {$set: {unresolvedErrors: countCardset}});
+				} else {
+					Meteor.call("updateUnresolvedErrors", id, card_id, cardset_id);
+				}
+			}
+			if (card_id !== "") {
+				const countCard = ErrorReporting.find({card_id: card_id, status: 0}).count();
+				Cards.update({_id: card_id}, {$set: {unresolvedErrors: countCard}});
+			}
 		}
 	},
 	getCardErrors: function (card_id) {
 		return ErrorReporting.find({card_id: card_id}, {sort: {status: 1}}).fetch();
 	},
 	getCardErrorsFromUser: function (card_id, user_id) {
-		return ErrorReporting.find({card_id: card_id,user_id: user_id, status: 0}).fetch();
+		return ErrorReporting.find({card_id: card_id, user_id: user_id, status: 0}).fetch();
 	},
 	hasErrorReportings: function () {
 		if (Meteor.isServer) {
